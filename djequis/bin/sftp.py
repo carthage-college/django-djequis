612b1d6c4d64c58ca61d53634f5f69ab7cf59b9c
plungerman <plungerman@gmail.com>
2017-01-02 19:57:16 -0600

    simple shell script to test sftp connection to terradotta server

---
+++ djequis/bin/sftp.py
@@ -0,0 +1,37 @@
+import io
+import os
+import re
+import sys
+import pysftp
+
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+sys.path.append('/usr/lib/python2.7/')
+sys.path.append('/usr/local/lib/python2.7/dist-packages/')
+sys.path.append('/data2/django_1.8/')
+sys.path.append('/data2/django_projects/')
+sys.path.append('/data2/django_third/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
+
+from django.conf import settings
+
+
+def main():
+    # go to our storage directory on this server
+    os.chdir('/data2/www/data/terradotta/')
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    XTRNL_CONNECTION = {
+    'host':'sftp-us-03.terradotta.com', 'username':'CARTHAGE-SA',
+    'private_key':'/data2/www/keys/terradotta_rsa', 'password':'HRDxt3Qp?DJ+jG5&',
+    'cnopts':cnopts
+    }
+    # transfer the CSV to scripsafe
+    with pysftp.Connection(**XTRNL_CONNECTION) as sftp:
+        sftp.put("sis_hr_user_info.txt", preserve_mtime=True)
+
+
+    print "files sent to script safe:\n{}".format(philes)
+
+if __name__ == "__main__":
+
+    sys.exit(main())
