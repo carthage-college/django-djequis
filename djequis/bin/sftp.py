38d296fded6fe2228a604443c14c170f8e3304ea
plungerman <plungerman@gmail.com>
2017-01-05 08:25:29 -0600

    swapped out terradotta for everbridge values

--- djequis/bin/sftp.py
+++ djequis/bin/sftp.py
@@ -10,16 +10,20 @@ sys.path.append('/usr/local/lib/python2.7/dist-packages/')
 sys.path.append('/data2/django_1.8/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djequis.settings')
 
 from django.conf import settings
 
 
 def main():
     # go to our storage directory on this server
-    os.chdir('/data2/www/data/terradotta/')
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
+    #os.chdir('/data2/www/data/terradotta/')
+    #filename = 'sis_hr_user_info.txt'
+    os.chdir('/data2/www/data/everbridge/')
+    filename = 'Student-20170105081228.csv'
+    #cnopts = pysftp.CnOpts()
+    #cnopts.hostkeys = None
+    '''
     XTRNL_CONNECTION = {
         'host':settings.TERRADOTTA_HOST,
         'username':settings.TERRADOTTA_USER,
@@ -27,12 +31,19 @@ def main():
         'private_key_pass':settings.TERRADOTTA_PASS,
         'cnopts':cnopts
     }
+    '''
+    XTRNL_CONNECTION = {
+        'host':settings.EVERBRIDGE_HOST,
+        'username':settings.EVERBRIDGE_USER,
+        'private_key':settings.EVERBRIDGE_PKEY,
+        'private_key_pass':settings.EVERBRIDGE_PASS,
+    }
     # transfer the CSV to scripsafe
     with pysftp.Connection(**XTRNL_CONNECTION) as sftp:
-        sftp.put("sis_hr_user_info.txt", preserve_mtime=True)
-        
+        sftp.put(filename, preserve_mtime=True)
+
     print "done"
-    
-if __name__ == "__main__":
+
+if __name__ == '__main__':
 
     sys.exit(main())
