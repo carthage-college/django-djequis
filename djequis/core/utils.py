973e8f510ece3fcebd6ee5d6b21c5b59a09fbfe4
plungerman <plungerman@gmail.com>
2017-01-16 08:12:46 -0600

    added debug flag for sending email

--- djequis/core/utils.py
+++ djequis/core/utils.py
@@ -2,16 +2,18 @@ import smtplib
 import email.utils
 from email.mime.text import MIMEText
 
-def sendmail(to, frum, body, subject):
+def sendmail(to, frum, body, subject, debug=False):
 
     # Create the message
     msg = MIMEText(body)
     msg['To'] = email.utils.formataddr(('Recipient', to))
-    msg['From'] = email.utils.formataddr(('DJEquis', frum))
+    msg['From'] = email.utils.formataddr(('DJ Equis', frum))
     msg['Subject'] = subject
 
     server = smtplib.SMTP('localhost')
-    server.set_debuglevel(True) # show communication with the server
+    # show communication with the server
+    if debug:
+        server.set_debuglevel(True)
     try:
         server.sendmail(frum, to, msg.as_string())
     finally:
