6e3defd18897b7e85e39b41badd1795b9488be4e
ssmolik <ssmolik@carthage.edu>
2017-06-01 09:49:00 -0500

    initial checkin

---
+++ djequis/bin/barnesandnoble.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import pysftp
+import csv
+import time
+
+# python path
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+sys.path.append('/usr/lib/python2.7/')
+sys.path.append('/usr/local/lib/python2.7/dist-packages/')
+sys.path.append('/data2/django_1.11/')
+sys.path.append('/data2/django_projects/')
+sys.path.append('/data2/django_third/')
+
+# django settings for shell environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
+
+# prime django
+import django
+django.setup()
+
+# django settings for script
+from django.conf import settings
+
+# informix environment
+os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
+os.environ['DBSERVERNAME'] = settings.DBSERVERNAME
+os.environ['INFORMIXDIR'] = settings.INFORMIXDIR
+os.environ['ODBCINI'] = settings.ODBCINI
+os.environ['ONCONFIG'] = settings.ONCONFIG
+os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
+os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
+os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
+
+from djequis.sql.barnesandnoble import STUDENT_UPLOAD
+from djequis.sql.barnesandnoble import ADULT_UPLOAD
+from djequis.sql.barnesandnoble import FACSTAFF_UPLOAD
+from djequis.core.utils import sendmail
+from djzbar.utils.informix import do_sql
+
+EARL = settings.INFORMIX_EARL
+
+desc = """
+    Barnes and Noble Upload
+"""
+
+def main():
+
+
+if __name__ == "__main__":
+
+    sys.exit(main())
