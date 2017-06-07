80834e4bf7a89cd898ca2b4c14e03468beea0630
plungerman <plungerman@gmail.com>
2017-06-07 14:06:08 -0500

    code clean up

--- djequis/bin/skel.py
+++ djequis/bin/skel.py
@@ -7,7 +7,7 @@ sys.path.append('/usr/local/lib/python2.7/dist-packages/')
 sys.path.append('/data2/django_current/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djequis.settings')
 
 # required if using django models
 import django
@@ -17,9 +17,9 @@ from django.conf import settings
 
 import argparse
 
-"""
+'''
 Shell script...
-"""
+'''
 
 # set up command-line options
 desc = """
@@ -29,28 +29,28 @@ Accepts as input...
 parser = argparse.ArgumentParser(description=desc)
 
 parser.add_argument(
-    "-x", "--equis",
+    '-x', '--equis',
     required=True,
     help="Lorem ipsum dolor sit amet.",
-    dest="equis"
+    dest='equis'
 )
 parser.add_argument(
-    "--test",
+    '--test',
     action='store_true',
     help="Dry run?",
-    dest="test"
+    dest='test'
 )
 
 def main():
-    """
+    '''
     main function
-    """
+    '''
 
 ######################
 # shell command line
 ######################
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parser.parse_args()
     equis = args.equis
     test = args.test
