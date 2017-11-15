56877ccf20cd874b299ceac1baec2fb1f7de6287
drobinson <drobinson@carthage.edu>
2017-11-15 11:02:43 -0600

    moved class_year.py and remoevd jx/

--- djequis/core/urls.py
+++ djequis/core/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(
         r'^admissions/', include('djequis.core.admissions.urls')
     ),
+
+
     # test app
     url(
         r'^test/', include('djequis.core.test.urls')
