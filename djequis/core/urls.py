df57592dad501c1cd8c15f67497c5f12c8e20c7a
drobinson <drobinson@carthage.edu>
2017-11-15 10:19:22 -0600

    removed the registrar app

--- djequis/core/urls.py
+++ djequis/core/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     url(
         r'^admissions/', include('djequis.core.admissions.urls')
     ),
-    # registrar
-    url(
-        r'^registrar/', include('djequis.core.registrar.urls')
-    ),
     # test app
     url(
         r'^test/', include('djequis.core.test.urls')
