1a6fe3d2cbf3394a7d6a5b752ee642a812a7ca01
ssmolik <ssmolik@carthage.edu>
2018-01-19 11:44:39 -0600

    added try / except to code

--- djequis/bin/papercut_glrec.py
+++ djequis/bin/papercut_glrec.py
@@ -2,9 +2,7 @@ import os
 import sys
 import csv
 import argparse
-#from datetime import datetime
 import time
-#from time import gmtime, strftime
 import shutil
 
 # python path
@@ -51,7 +49,7 @@ EARL = settings.INFORMIX_EARL
 
 # set up command-line options
 desc = """
-    GL Account Names for Papercut
+    Papercut Account Names from GL
 """
 parser = argparse.ArgumentParser(description=desc)
 
@@ -65,7 +63,7 @@ def main():
     # execute SQL statement
     sqlresult = do_sql(GET_GL_ACCTS, earl=EARL)
     # formatting date and time string 
-    datetimestr = time.strftime("%Y%m%d%H%M%S")
+    datetimestr = time.strftime("%Y-%m-%d")
     # set directory and filename
     filename = ('{0}glrec_data.csv'.format(settings.PAPERCUT_CSV_OUTPUT))
     # set destination path and new filename that it will be renamed to when archived
@@ -75,35 +73,42 @@ def main():
     # opens a file for writing
     with open(filename,'w') as glrecfile:
         for row in sqlresult:
-            #print(row)
-            #acct_desc = row['acct_descr'].split('/',1)[1]
-            # creates a formatted string
-            accountName = ("{0}/{1}-{2}-{3}".format(row['acct_descr'].split('/',1)[1],
-                                                 row['fund'],row['func'],row['obj'])
-                        )
-            print(accountName)
-            # writes a formatted string to the file
-            glrecfile.write('{0}\n'.format(accountName))
+            try:
+                # creates a formatted string
+                accountName = ("{0}/{1}-{2}-{3}".format(row['acct_descr'].split('/',1)[1],
+                                                row['fund'],row['func'],row['obj'])
+                            )
+                # writes a formatted string to the file
+                glrecfile.write('{0}\n'.format(accountName))
+            except Exception as e:
+                print "Exception: {0}".format(str(e))
+                # Email that there was an exception error while processing .csv
+                SUBJECT = '[Papercut] GL Exception Error'
+                BODY = "There was an exception error: {0}".format(str(e))
+                sendmail(
+                    settings.PAPERCUT_TO_EMAIL,settings.PAPERCUT_FROM_EMAIL,
+                    BODY, SUBJECT
+                )
     # close file
     glrecfile.close()
-
+    # removed for now S. Smolik 1/19/2018
     # sends email attachment
-    file_attach = '{0}glrec_data.csv'.format(settings.PAPERCUT_CSV_OUTPUT)
-    request = None
-    recipients = settings.PAPERCUT_TO_EMAIL
-    subject = "[GL Account Names] attachment"
-    femail = settings.PAPERCUT_FROM_EMAIL
-    template = 'papercut/glrec_email.html'
-    bcc = 'ssmolik@carthage.edu'
-    send_mail(
-        request, recipients, subject, femail, template, bcc, attach=file_attach
-    )
+    # file_attach = '{0}glrec_data.csv'.format(settings.PAPERCUT_CSV_OUTPUT)
+    # request = None
+    # recipients = settings.PAPERCUT_TO_EMAIL
+    # subject = "[GL Account Names] attachment"
+    # femail = settings.PAPERCUT_FROM_EMAIL
+    # template = 'papercut/glrec_email.html'
+    # bcc = 'ssmolik@carthage.edu'
+    # send_mail(
+    #     request, recipients, subject, femail, template, bcc, attach=file_attach
+    # )
     # renaming old filename to newfilename and move to archive location
     shutil.copy(filename, archive_destination)
     # sleep for 3 seconds
-    #time.sleep(3)
+    time.sleep(3)
     # delete file
-    #os.remove(filename)
+    os.remove(filename)
 
 if __name__ == "__main__":
     args = parser.parse_args()
