15b0023b60de73d5eb316662b4adc5b17176090b
plungerman <plungerman@gmail.com>
2018-02-14 11:49:06 -0600

    modified the web interface so that the user just downloads a CSV file rather than seeing the data displayed in a table and then generating the CSV file when they click on the datatables button

--- djequis/core/financialaid/views.py
+++ djequis/core/financialaid/views.py
@@ -1,15 +1,17 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
 
 from djequis.core.financialaid.forms import WisAct284Form
 from djequis.sql.wisact284 import WIS_ACT_284_SQL
+from djequis.core.financialaid.utils import csv_gen
 
 from djzbar.decorators.auth import portal_auth_required
 from djzbar.utils.informix import do_sql
 
+import csv
+import time
 import logging
 logger = logging.getLogger(__name__)
 
@@ -35,6 +37,22 @@ def wisact284(request):
             sql = WIS_ACT_284_SQL(amt_stat = stat)
             logger.debug("wisact284 sql = {}".format(sql))
             objects = do_sql(sql, earl=EARL)
+
+            datetimestr = time.strftime("%Y%m%d%H%M%S")
+            # College Cost Meter file name
+            ccmfile = (
+                'CCM-{}.csv'.format(datetimestr)
+            )
+
+            response = HttpResponse(content_type="text/csv; charset=utf-8")
+            content = 'attachment; filename={}'.format(ccmfile)
+            response['Content-Disposition'] = content
+
+            writer = csv.writer(response)
+            csv_gen(objects, writer)
+
+            return response
+
     else:
         form = WisAct284Form()
 
