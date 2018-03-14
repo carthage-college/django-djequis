a9b617402f563ae3b0c90394ab10742811605aad
ssmolik <ssmolik@carthage.edu>
2018-03-14 10:14:55 -0500

    updated the sql for the users query

--- djequis/sql/schoology.py
+++ djequis/sql/schoology.py
@@ -1,160 +1,158 @@
-# fetch courses
+# fetch courses and sections
+# This query should return all courses and sections active from July to July of the current fiscal year.
+# Based on the dates for the terms courses and sections are made active or inactive automatically.
 COURSES = '''
-    select 
-    trim(jenzccd_rec.title) Coursename,  
-    trim(jenzdpt_rec.descr) Department,
-    trim(jenzcrs_rec.course_code) CourseCode,  
+    SELECT 
+    TRIM(jenzccd_rec.title) Coursename,
+    TRIM(jenzdpt_rec.descr) Department,
+    TRIM(jenzcrs_rec.course_code) CourseCode,
     jenzcrs_rec.hrs Credits, 
-    trim(jenzccd_rec.title)||'-'||trim(jenzcrs_rec.sec)||' '||case when trim(x) = '------- 0-0' and trim(y) = '------- 0-0' then ''
-         when trim(x) = trim(y) then x 
-         else  x||' / '||y end  descr,
-    --jenzccd_rec.descr descr,
-    trim(jenzcrs_rec.sec)||'-'||trim(InstrName)||' '||case when trim(x) = '------- 0-0' and trim(y) = '------- 0-0' then ''
-         when trim(x) = trim(y) then x 
-         else  x||' / '||y end SectionName, 
-    trim(jenzcrs_rec.coursekey) SecSchoolCode, 
-    left(jenzcrs_rec.course_code,8)||'-'||trim(jenzcrs_rec.sec)||' '||trim(jenzcrs_rec.term_code) SectionCode,  -- Use this one
-    case when trim(x) = '------- 0-0' and trim(y) = '------- 0-0' then ''
-         when trim(x) = trim(y) then x 
-         else  x||' / '||y end as SecDescr, 
-    trim(bldg)||' '||trim(ROOM) location,
+    TRIM(jenzccd_rec.title)||'-'||TRIM(jenzcrs_rec.sec)||' '||
+        CASE WHEN trim(x) = '------- 0-0' AND TRIM(y) = '------- 0-0' THEN ''
+             WHEN trim(x) = TRIM(y) THEN x ELSE  x||' / '||y END  descr,
+    TRIM(jenzcrs_rec.sec)||'-'||TRIM(InstrName)||' '||
+        CASE WHEN trim(x) = '------- 0-0' AND TRIM(y) = '------- 0-0' THEN ''
+             WHEN TRIM(x) = TRIM(y) THEN x ELSE  x||' / '||y END SectionName, 
+    TRIM(jenzcrs_rec.coursekey) SecSchoolCode, 
+    LEFT(jenzcrs_rec.course_code,8)||'-'||TRIM(jenzcrs_rec.sec)||' '||TRIM(jenzcrs_rec.term_code) SectionCode,
+    CASE WHEN TRIM(x) = '------- 0-0' AND TRIM(y) = '------- 0-0' THEN ''
+         WHEN TRIM(x) = TRIM(y) THEN x ELSE  x||' / '||y END AS SecDescr, 
+    TRIM(bldg)||' '||trim(ROOM) location,
     'Carthage College' School,
-    trim(jenzcrs_rec.term_code) GradingPeriod
-    from jenzcrs_rec 
-    join crs_rec on trim(jenzcrs_rec.course_code) = trim(crs_rec.crs_no)||' ('||trim(crs_rec.cat)||')'
-    join Jenzccd_rec on Jenzccd_rec.course_code = jenzcrs_rec.course_code 
-    join jenztrm_rec on jenztrm_rec.term_code = jenzcrs_rec.term_code
-    join jenzcrp_rec on jenzcrp_rec.course_code = jenzcrs_rec.course_code
-    and jenzcrp_rec.sec = jenzcrs_rec.sec 
+    TRIM(jenzcrs_rec.term_code) GradingPeriod
+    FROM jenzcrs_rec 
+    JOIN crs_rec on trim(jenzcrs_rec.course_code) = TRIM(crs_rec.crs_no)||' ('||TRIM(crs_rec.cat)||')'
+    JOIN Jenzccd_rec ON Jenzccd_rec.course_code = jenzcrs_rec.course_code 
+    JOIN jenztrm_rec ON jenztrm_rec.term_code = jenzcrs_rec.term_code
+    JOIN jenzcrp_rec ON jenzcrp_rec.course_code = jenzcrs_rec.course_code
+    AND jenzcrp_rec.sec = jenzcrs_rec.sec 
     AND jenzcrp_rec.term_code = jenzcrs_rec.term_code
-    join id_rec on id_Rec.id = jenzcrp_rec.host_id
-    ----left join disc_table on disc_table.disc = left(jenzcrs_rec.course_code,4)
-    left join jenzdpt_rec on jenzdpt_rec.dept_code = jenzccd_rec.dept_code
-    left join jenzsch_rec on jenzsch_rec.term_code = jenzcrs_rec.term_code
-    and jenzsch_rec.sec = jenzcrs_rec.sec
-    and jenzsch_rec.course_code = jenzcrs_rec.course_code
-    join secmtg_rec on secmtg_rec.crs_no = crs_rec.crs_no
-    and secmtg_rec.sec_no = jenzcrs_rec.sec
-    and trim(secmtg_rec.sess) = left(jenzcrs_rec.term_code,2) 
-    and secmtg_rec.yr =  SUBSTRING(jenzcrs_rec.term_code FROM 4 FOR 4)
-    and secmtg_rec.cat = crs_rec.cat
-    join (select a.crs_no, a.sec_no, a.cat, a.yr, a.sess, c.lastname as InstrName, c.firstname, c.fullname, a.fac_id
-    from sec_rec a, id_rec c
-    where c.id = a.fac_id) Instructor
-    on Instructor.sec_no = secmtg_rec.sec_no
-    and Instructor.crs_no = secmtg_rec.crs_no
-    and Instructor.cat = secmtg_rec.cat
-    and Instructor.yr = secmtg_rec.yr
-    and Instructor.sess = secmtg_rec.sess
-    join
+    JOIN id_rec on id_Rec.id = jenzcrp_rec.host_id
+    LEFT join jenzdpt_rec ON jenzdpt_rec.dept_code = jenzccd_rec.dept_code
+    LEFT join jenzsch_rec ON jenzsch_rec.term_code = jenzcrs_rec.term_code
+    AND jenzsch_rec.sec = jenzcrs_rec.sec
+    AND jenzsch_rec.course_code = jenzcrs_rec.course_code
+    JOIN secmtg_rec ON secmtg_rec.crs_no = crs_rec.crs_no
+    AND secmtg_rec.sec_no = jenzcrs_rec.sec
+    AND trim(secmtg_rec.sess) = left(jenzcrs_rec.term_code,2) 
+    AND secmtg_rec.yr =  SUBSTRING(jenzcrs_rec.term_code FROM 4 FOR 4)
+    AND secmtg_rec.cat = crs_rec.cat
+    JOIN (select a.crs_no, a.sec_no, a.cat, a.yr, a.sess, c.lastname as InstrName, c.firstname, c.fullname, a.fac_id
+    FROM sec_rec a, id_rec c
+    WHERE c.id = a.fac_id) Instructor
+    ON Instructor.sec_no = secmtg_rec.sec_no
+    AND Instructor.crs_no = secmtg_rec.crs_no
+    AND Instructor.cat = secmtg_rec.cat
+    AND Instructor.yr = secmtg_rec.yr
+    AND Instructor.sess = secmtg_rec.sess
+    JOIN
     (select  b.crs_no, b.yr, b.sec_no, b.cat, b.sess, c.txt as BLDG, a.room as ROOM,
-    max(a.mtg_no) as MaxMtgNo,
-    max(b.crs_no||'-'||b.sec_no||'-'||trim(days)||' '||cast(beg_tm as int)||'-'||cast(end_tm as int)) as x,
-    min(b.crs_no||'-'||b.sec_no||'-'||trim(days)||' '||cast(beg_tm as int)||'-'||cast(end_tm as int)) as y
-    from mtg_rec a, secmtg_rec b, bldg_table c
-    where a.mtg_no = b.mtg_no
-    and a.bldg = c.bldg
-    group by b.crs_no, b.yr, b.sec_no, b.cat, b.sess, c.txt, a.room ) MeetPattern
-    on MeetPattern.crs_no = crs_rec.crs_no
-    and MeetPattern.yr = left(jenzcrs_rec.coursekey,4)
-    and MeetPattern.MaxMtgNo = secmtg_rec.mtg_no
-    where jenztrm_rec.start_date >= add_months(today,-1) and jenztrm_rec.end_date <= add_months(today,+5)
-    --where jenztrm_rec.start_date < today and jenztrm_rec.end_date > today
-    and jenzcrp_rec.status_code = '1PR'
-    --and trim(jenzcrs_rec.course_code) = 'NEU 675R  S (UG17)'
-    and id_rec.id in
-    (select id from aa_rec
-    where aa in ('EML1')
-    and line1 in ('ahenle@carthage.edu', 'bcarlson1@carthage.edu', 'dbrownholland@carthage.edu', 'lchristoun@carthage.edu', 'apustina@carthage.edu',
-    'adassow@carthage.edu', 'bzopf@carthage.edu', 'dcooksnyder@carthage.edu', 'ewheeler@carthage.edu', 'aduncan@carthage.edu',
-    'jseymour@carthage.edu', 'ksconzert@carthage.edu', 'skonrad@carthage.edu', 'hyaple@carthage.edu',
-    'lhuaracha@carthage.edu', 'npilarski@carthage.edu', 'jshields@carthage.edu', 'dschowalter@carthage.edu', 'abarnhart@carthage.edu',
-    'fhicks@carthage.edu', 'cgrugel@carthage.edu', 'csabbar@carthage.edu'))
-    
-    order by jenzcrs_rec.term_code
+    MAX(a.mtg_no) AS MaxMtgNo,
+    MAX(b.crs_no||'-'||b.sec_no||'-'||TRIM(days)||' '||CAST(beg_tm as int)||'-'||CAST(end_tm as int)) AS x,
+    MIN(b.crs_no||'-'||b.sec_no||'-'||TRIM(days)||' '||CAST(beg_tm as int)||'-'||CAST(end_tm as int)) AS y
+    FROM mtg_rec a, secmtg_rec b, bldg_table c
+    WHERE a.mtg_no = b.mtg_no
+    AND a.bldg = c.bldg
+    GROUP BY b.crs_no, b.yr, b.sec_no, b.cat, b.sess, c.txt, a.room ) MeetPattern
+    ON MeetPattern.crs_no = crs_rec.crs_no
+    AND MeetPattern.yr = LEFT(jenzcrs_rec.coursekey,4)
+    AND MeetPattern.MaxMtgNo = secmtg_rec.mtg_no
+    WHERE jenztrm_rec.start_date >= add_months(today,-1) AND jenztrm_rec.end_date <= add_months(today,+5)
+    AND jenzcrp_rec.status_code = '1PR'
+    AND id_rec.id in
+    (SELECT id from aa_rec
+    WHERE aa IN ('EML1')
+    AND line1 IN ('ahenle@carthage.edu', 'bcarlson1@carthage.edu', 'dbrownholland@carthage.edu',
+    'lchristoun@carthage.edu', 'apustina@carthage.edu', 'adassow@carthage.edu',
+    'bzopf@carthage.edu', 'dcooksnyder@carthage.edu', 'ewheeler@carthage.edu',
+    'aduncan@carthage.edu', 'jseymour@carthage.edu', 'ksconzert@carthage.edu',
+    'skonrad@carthage.edu', 'hyaple@carthage.edu', 'lhuaracha@carthage.edu',
+    'npilarski@carthage.edu', 'jshields@carthage.edu', 'dschowalter@carthage.edu',
+    'abarnhart@carthage.edu', 'fhicks@carthage.edu', 'cgrugel@carthage.edu',
+    'csabbar@carthage.edu'))
+    ORDER BY jenzcrs_rec.term_code
 '''
 # fetch users
+# Users are collected in a single query to get both Students and Faculty/Staff.
+# The student query portion pulls all students with an academic record between
+# the start of the current fiscal year (July 1) and the end of the current fiscal year.
+# The Faculty/Staff portion should get all employees with active job records within the last year.
+# There are enrollments for individuals who are not currently staff or faculty.
+# If I filter to only users who were employed in the last year, I find enrollment records without a matching user. 
 USERS = '''
-    select distinct id_rec.firstname, addree_rec.alt_name preferred_first_name, id_rec.middlename, id_rec.lastname, 
-    id_rec.title name_prefix, 
-    trim(jenzprs_rec.host_username) username, TRIM(jenzprs_rec.e_mail) EMAIL,
-    jenzprs_rec.host_id UniqueID, 
-    jenzcst_rec.status_code role, 
-    'Carthage College' school, 
-    jenzprs_rec.host_id schoology_id, 
-    CASE NVL(title1.job_title,'x') WHEN 'x' then '' ELSE trim(title1.job_title) END||  
-    CASE NVL(title2.job_title,'x') WHEN 'x' then '' ELSE '; '||trim(title2.job_title) END||  
-    CASE NVL(title3.job_title,'x') WHEN 'x' then '' ELSE '; '||trim(title3.job_title) END 
-    Postition,
-    '' pwd, '' gender, '' GradYr, '' additional_schools
-    from jenzprs_rec
-    join jenzcst_rec 
-        on jenzprs_rec.host_id = jenzcst_rec.host_id
-        and jenzcst_rec.status_code in ('FAC','STU')
-    left join id_rec on id_rec.id =  jenzprs_rec.host_id
-    left join job_rec title1 on title1.id = jenzprs_rec.host_id and title1.title_rank = 1 and (title1.end_date is null or title1.end_date > current) 
-    left join job_rec title2 on title2.id = jenzprs_rec.host_id and title2.title_rank = 2 and (title2.end_date is null or title2.end_date > current)
-    left join job_rec title3 on title3.id = jenzprs_rec.host_id and title3.title_rank = 3 and (title3.end_date is null or title3.end_date > current)
-    left join addree_rec on addree_rec.prim_id = jenzprs_rec.host_id and addree_rec.style = 'N'
-    --This will filter users to those who have enrollments in the selected time frame
-    and jenzprs_rec.host_id in 
-    (select ID from
-    (select distinct ID from job_rec, hrdept_table, hrstat_table
-    where job_rec.hrdept = hrdept_table.hrdept 
-    and hrstat_table.hrstat = job_rec.hrstat
-    and hrstat_table.inactive_date is null
-    and job_rec.end_date is null and hrdept_table.end_date is null
-    and hrpay not in ('VEN', 'DPW', 'GRD','K97', 'SUM')
-    and hrstat_table.hrstat not in ('LV', 'DA', 'SA', 'STU', 'HR', 'HRPT', 'VEND') )
-    union
-    (select id from
-    stu_acad_rec ACADREC inner join acad_cal_rec CALREC on CALREC.sess = ACADREC.sess and CALREC.yr = ACADREC.yr and CALREC.prog = ACADREC.prog 
-    and CALREC.acyr = CASE WHEN TODAY < TO_DATE(YEAR(TODAY) || '-07-01', '%Y-%m-%d')	THEN MOD(YEAR(TODAY) - 1, 100) || MOD(YEAR(TODAY), 100)
-                                ELSE MOD(YEAR(TODAY), 100) || MOD(YEAR(TODAY) + 1, 100)
-                                END) )
-    and (addree_rec.inactive_date is null or addree_rec.inactive_date > current) 
-    --and id_rec.id in (729329) 
-    --E:   This does prevent a small number of records from pulling if enabled
-    order by jenzprs_rec.host_id
-    --If an individual is both FAC and STU this will create two records.
-    --May have to use another table to determine role
+    SELECT DISTINCT
+        id_rec.firstname, addree_rec.alt_name preferred_first_name, id_rec.middlename,
+        id_rec.lastname, id_rec.title name_prefix, trim(jenzprs_rec.host_username) username,
+        TRIM(jenzprs_rec.e_mail) EMAIL, jenzprs_rec.host_id UniqueID,
+        CASE WHEN jenzcst_rec.STATUS_CODE = 'STU' THEN 'STU' ELSE 'FAC' END AS ROLE,
+        'Carthage College' school, 
+        jenzprs_rec.host_id schoology_id,
+        CASE NVL(title1.job_title,'x') WHEN 'x' THEN '' ELSE trim(title1.job_title) END||
+        CASE NVL(title2.job_title,'x') WHEN 'x' THEN '' ELSE '; '||trim(title2.job_title) END||
+        CASE NVL(title3.job_title,'x') WHEN 'x' THEN '' ELSE '; '||trim(title3.job_title) END 
+        Position, '' pwd, '' gender, '' GradYr, '' additional_schools
+    FROM jenzprs_rec
+    LEFT join jenzcst_rec
+    ON jenzprs_rec.host_id = jenzcst_rec.host_id
+    AND jenzcst_rec.status_code IN ('FAC', 'STF', 'STU', 'ADM')
+        JOIN id_rec on id_rec.id =  jenzprs_rec.host_id
+        LEFT JOIN job_rec title1 ON title1.id = jenzprs_rec.host_id AND title1.title_rank = 1
+            AND (title1.end_date IS NULL OR title1.end_date > current) 
+        LEFT JOIN job_rec title2 ON title2.id = jenzprs_rec.host_id AND title2.title_rank = 2
+            AND (title2.end_date IS NULL OR title2.end_date > current)
+        LEFT JOIN job_rec title3 ON title3.id = jenzprs_rec.host_id AND title3.title_rank = 3
+            AND (title3.end_date IS NULL OR title3.end_date > current)
+        LEFT JOIN job_rec title4 ON title4.id = jenzprs_rec.host_id AND title4.title_rank IS NULL
+            AND (title4.end_date IS NULL OR title4.end_date > current)
+        LEFT JOIN addree_rec ON addree_rec.prim_id = jenzprs_rec.host_id AND addree_rec.style = 'N'
+    WHERE jenzprs_rec.host_id IN
+        (
+            select to_number(host_id) AS UID
+                FROM jenzcrp_rec
+                UNION All
+            SELECT cx_id AS UID
+                FROM provsndtl_rec
+                WHERE subsys = 'MSTR'
+                    AND action = 'Active'
+                    AND roles NOT IN ('Contractor')
+        )
+    ORDER BY id_rec.lastname, id_rec.firstname;
 '''
 # fetch enrollment
+# This query should return all instructors and students enrolled in active courses July-July for the current fiscal year.
 ENROLLMENT = '''
-    select 
-    jenzcrp_rec.course_code CourseCode, 
-    left(jenzcrs_rec.course_code,8)||'-'||trim(jenzcrs_rec.sec)||' '||trim(jenzcrs_rec.term_code) SectionCode,
-    jenzcrs_rec.coursekey SecSchoolCode,
-    jenzcrp_rec.host_id UniqueUserID, 
-    jenzcrp_rec.status_code EnrollmentType, 
-    jenzcrp_rec.term_code GradePeriod
-    from jenzcrp_rec
-    join 
-    jenzcrs_rec on jenzcrp_rec.course_code = jenzcrs_rec.course_code 
-    and jenzcrp_rec.sec = jenzcrs_rec.sec 
+    SELECT
+        jenzcrp_rec.course_code CourseCode,
+        left(jenzcrs_rec.course_code,8)||'-'||trim(jenzcrs_rec.sec)||' '||trim(jenzcrs_rec.term_code) SectionCode,
+        jenzcrs_rec.coursekey SecSchoolCode, jenzcrp_rec.host_id UniqueUserID,
+        jenzcrp_rec.status_code EnrollmentType, jenzcrp_rec.term_code GradePeriod
+    FROM jenzcrp_rec
+        JOIN jenzcrs_rec on jenzcrp_rec.course_code = jenzcrs_rec.course_code
+    AND jenzcrp_rec.sec = jenzcrs_rec.sec
     AND jenzcrp_rec.term_code = jenzcrs_rec.term_code
-    join jenztrm_rec on jenztrm_rec.term_code = jenzcrs_rec.term_code
-    --and trim(jenzcrs_rec.course_code) = 'NEU 675R  S (UG17)'
-    where jenzcrs_rec.coursekey in (
-    select 
-    trim(jenzcrs_rec.coursekey) SecSchoolCode
-    from jenzcrs_rec 
-    join crs_rec on trim(jenzcrs_rec.course_code) = trim(crs_rec.crs_no)||' ('||trim(crs_rec.cat)||')'
-    join jenztrm_rec on jenztrm_rec.term_code = jenzcrs_rec.term_code
-    join jenzcrp_rec on jenzcrp_rec.course_code = jenzcrs_rec.course_code
-    and jenzcrp_rec.sec = jenzcrs_rec.sec 
-    AND jenzcrp_rec.term_code = jenzcrs_rec.term_code
-    join id_rec on id_Rec.id = jenzcrp_rec.host_id
-    where jenztrm_rec.start_date >= add_months(today,-1) and jenztrm_rec.end_date <= add_months(today,+5)
-    --where jenztrm_rec.start_date < today and jenztrm_rec.end_date > today
-    and jenzcrp_rec.status_code = '1PR'
-    and id_rec.id in
-    (select id from aa_rec
-    where aa in ('EML1')
-    and line1 in ('ahenle@carthage.edu', 'bcarlson1@carthage.edu', 'dbrownholland@carthage.edu', 'lchristoun@carthage.edu', 'apustina@carthage.edu',
-    'adassow@carthage.edu', 'bzopf@carthage.edu', 'dcooksnyder@carthage.edu', 'ewheeler@carthage.edu', 'aduncan@carthage.edu',
-    'jseymour@carthage.edu', 'ksconzert@carthage.edu', 'skonrad@carthage.edu', 'hyaple@carthage.edu',
-    'lhuaracha@carthage.edu', 'npilarski@carthage.edu', 'jshields@carthage.edu', 'dschowalter@carthage.edu', 'abarnhart@carthage.edu',
-    'fhicks@carthage.edu', 'cgrugel@carthage.edu', 'csabbar@carthage.edu')))
-    order by coursecode
+        JOIN jenztrm_rec on jenztrm_rec.term_code = jenzcrs_rec.term_code
+    WHERE jenzcrs_rec.coursekey in
+        (
+        SELECT 
+            TRIM(jenzcrs_rec.coursekey) SecSchoolCode
+        FROM jenzcrs_rec
+            JOIN crs_rec on trim(jenzcrs_rec.course_code) = trim(crs_rec.crs_no)||' ('||trim(crs_rec.cat)||')'
+            JOIN jenztrm_rec on jenztrm_rec.term_code = jenzcrs_rec.term_code
+            JOIN jenzcrp_rec on jenzcrp_rec.course_code = jenzcrs_rec.course_code
+        AND jenzcrp_rec.sec = jenzcrs_rec.sec
+        AND jenzcrp_rec.term_code = jenzcrs_rec.term_code
+            JOIN id_rec on id_Rec.id = jenzcrp_rec.host_id
+        WHERE jenztrm_rec.start_date >= add_months(today,-1) and jenztrm_rec.end_date <= add_months(today,+5)
+        AND jenzcrp_rec.status_code = '1PR'
+        AND id_rec.id IN
+            (select id from aa_rec
+            WHERE aa IN ('EML1')
+            AND line1 IN ('ahenle@carthage.edu', 'bcarlson1@carthage.edu', 'dbrownholland@carthage.edu', 'lchristoun@carthage.edu', 'apustina@carthage.edu',
+            'adassow@carthage.edu', 'bzopf@carthage.edu', 'dcooksnyder@carthage.edu', 'ewheeler@carthage.edu', 'aduncan@carthage.edu',
+            'jseymour@carthage.edu', 'ksconzert@carthage.edu', 'skonrad@carthage.edu', 'hyaple@carthage.edu',
+            'lhuaracha@carthage.edu', 'npilarski@carthage.edu', 'jshields@carthage.edu', 'dschowalter@carthage.edu', 'abarnhart@carthage.edu',
+            'fhicks@carthage.edu', 'cgrugel@carthage.edu', 'csabbar@carthage.edu')
+            )
+        )
+    ORDER BY coursecode
 '''
