e11ca090e6ffc25917c3c6507b0f5730a85e66dc
plungerman <plungerman@gmail.com>
2018-04-26 12:07:33 -0500

    solidifying unit tests before proceeding with the grade conversion from numeric to grading scale string

--- djequis/core/schoology/tests/test_views.py
+++ djequis/core/schoology/tests/test_views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from djequis.core.schoology.sql import SELECT_GRADE, UPDATE_GRADE
+
 from djtools.utils.logging import seperator
 from djzbar.utils.informix import get_session
 
@@ -21,6 +23,9 @@ class CoreSchoologyViewsTestCase(TestCase):
         self.json_data = json.load(
             open(settings.SCHOOLOGY_TEST_GRADES_TRIGGER_JSON_FILE)
         )
+        self.json_data_bad = json.load(
+            open(settings.SCHOOLOGY_TEST_GRADES_TRIGGER_JSON_FILE_BAD)
+        )
 
     def test_trigger_grades_json(self):
 
@@ -29,9 +34,8 @@ class CoreSchoologyViewsTestCase(TestCase):
         print(seperator())
 
         data = self.json_data
-        print(data)
-
-        print(data['uid'])
+        # simple check for one json parameter
+        self.assertIn(data['type'], 'grades.update')
 
     def test_trigger_grades_view(self):
 
@@ -39,28 +43,90 @@ class CoreSchoologyViewsTestCase(TestCase):
         print("trigger grades view")
         print(seperator())
 
+        response = self.client.post(
+            reverse('trigger_grades'), json.dumps(self.json_data),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'Success')
+
+    def test_trigger_grades_update(self):
+
+        print("\n")
+        print("trigger grades update informix")
+        print(seperator())
+
         data = self.json_data
 
-
-        response = self.client.post(
-            reverse('trigger_grades'), data
-        )
-
-        print(response)
-
-        print("update informix")
-
-        # create the ctc_blob object with the value of the message body for txt
         session = get_session(self.earl)
 
-        sql = 'select * from jenzcrs_rec limit 100'
+        for grade in data['data']:
+            # 01234567890123456789012345678
+            # 2018;RC;FRN 2010;01;UG17;UNDG
+            # yr  sess crs_no  sec cat prog
+            school_code = grade['section_school_code']
 
-        print("insert sql statement:\n{}".format(sql))
-        logger.debug('sql = {}'.format(sql))
+            # use the --debug-mode flag to test with SELECT
+            if settings.DEBUG:
+                sql = SELECT_GRADE(
+                    coursekey = school_code,
+                    student_number = grade['school_uid']
+                )
+            else:
+                sql = UPDATE_GRADE(
+                    grade = grade['updated_overall_grade'],
+                    coursekey = school_code,
+                    student_number = grade['school_uid']
+                )
 
+            print("sql = {}".format(sql))
 
-        objects = session.execute(sql)
-        for o in objects:
-            print o
+            print("2018;RC;FRN 2010;01;UG17;UNDG")
+            print("yr  sess crs_no  sec cat prog")
+            print("cw_rec.yr = {}".format(school_code[0:4]))
+            print("cw_rec.cat = {}".format(school_code[20:25]))
+            print("cw_rec.sess = {}".format(school_code[5:7]))
+            print("cw_rec.prog = {}".format(school_code[25:]))
+            print("cw_rec.crs_no = {}".format(school_code[8:16]))
+            print("cw_rec.sec = {}".format(school_code[17:19]))
 
-        #session.commit()
+            try:
+                response = session.execute(sql)
+                if settings.DEBUG:
+                    for r in response:
+                        print(r)
+            except:
+                logger.debug('test_trigger_grades_update_bad_data = {}'.format(
+                    grade
+                ))
+
+        session.close()
+
+    def test_trigger_grades_update_bad_data(self):
+
+        print("\n")
+        print("trigger grades update informix with bad data")
+        print(seperator())
+
+        data = self.json_data_bad
+ 
+        session = get_session(self.earl)
+
+        for grade in data['data']:
+
+            try:
+                school_code = grade['section_school_code']
+                sql = UPDATE_GRADE(
+                    grade = grade['updated_overall_grade'],
+                    coursekey = school_code,
+                    student_number = grade['school_uid']
+                )
+
+                print("update grade sql = {}".format(sql))
+
+                response = session.execute(sql)
+            except:
+                logger.debug('test_trigger_grades_update_bad_data = {}'.format(
+                    grade
+                ))
+
