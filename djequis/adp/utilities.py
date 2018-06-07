5cf5b1b02554c73490d13720371968e7181ab6d9
dsullivan <dsullivan@carthage.edu>
2018-06-07 09:09:04 -0500

    Begin testing of sql insert and updates against a mysql database

--- djequis/adp/utilities.py
+++ djequis/adp/utilities.py
@@ -17,7 +17,6 @@ import shutil
 import re
 import logging
 from logging.handlers import SMTPHandler
-#import adp_ftp
 import codecs
 
 # python path
@@ -36,7 +35,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
 import django
 django.setup()
 
-# django settings for script
+# # django settings for script
 from django.conf import settings
 from django.db import connections
 
@@ -50,7 +49,7 @@ os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
 os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
 os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
 
-# from djequis.core.utils import sendmail
+from djequis.core.utils import sendmail
 from djzbar.utils.informix import do_sql
 from djzbar.utils.informix import get_engine
 from djzbar.settings import INFORMIX_EARL_TEST
@@ -84,6 +83,7 @@ global EARL
 #    EARL = INFORMIX_EARL_PROD
 # elif database == 'train':
 EARL = INFORMIX_EARL_TEST
+# EARL = "default"
 # else:
     # this will raise an error when we call get_engine()
     # below but the argument parser should have taken
@@ -95,6 +95,8 @@ engine = get_engine(EARL)
 #########################################################
 # Common function to validate that a record exists
 #########################################################
+
+
 def fn_validate_field(searchval, keyfield, retfield, table, keytype):
     if keytype == "char":
         qval_sql = "SELECT DISTINCT " + retfield + " FROM " + table \
@@ -103,22 +105,26 @@ def fn_validate_field(searchval, keyfield, retfield, table, keytype):
         qval_sql = "SELECT DISTINCT " + retfield + " FROM " + table \
                    + " WHERE " + keyfield + " = " + str(searchval)
 
-    # print(qval_sql)
+    print(qval_sql)
 
-    try:
-        sql_val = do_sql(qval_sql, earl=EARL)
-        if sql_val != None:
-            row = sql_val.fetchone()
-
-            if row == None:
-                return ("")
-            else:
-                return (row[0])
-        else:
-            return ("")
-
-    except Exception as e:
-        print(e)
+    # try:
+    #     # sql_val = do_sql(qval_sql, key=DEBUG, earl=EARL)
+    #     if sql_val != None:
+    #         row = sql_val.fetchone()
+    #         if row == None:
+    #             if keytype == "char":
+    #                 return ""
+    #             else:
+    #                 return 0
+    #         else:
+    #             return (row[0])
+    #     else:
+    #         if keytype == "char":
+    #             return ""
+    #         else:
+    #             return 0
+    # except Exception as e:
+    #     print(e)
 
 
 def fn_check_duplicates(searchval, keyfield, retfield, table, testval, keytype):
@@ -134,10 +140,9 @@ def fn_check_duplicates(searchval, keyfield, retfield, table, testval, keytype):
     print(qval_sql)
 
     try:
-        sql_val = do_sql(qval_sql, earl=EARL)
+        sql_val = do_sql(qval_sql, key=DEBUG, earl=EARL)
         if sql_val != None:
             row = sql_val.fetchone()
-
             if row == None:
                 return (0)
             else:
@@ -152,146 +157,83 @@ def fn_check_duplicates(searchval, keyfield, retfield, table, testval, keytype):
 #########################################################
 # Specific function to deal with email in aa_rec
 #########################################################
-def fn_set_email2(email, id, fullname):
-    q_check_email = '''
-                  SELECT aa_rec.aa, aa_rec.id, aa_rec.line1, 
-                  aa_rec.aa_no, aa_rec.beg_date 
-                  FROM aa_rec
-                  WHERE aa_rec.id = {0}
-                  AND aa_rec.aa = 'EML2' 
-                  AND aa_rec.end_date IS NULL
-                  '''.format(id)
-    print(q_check_email)
-    # logger.info("Select email info from aa_rec table");
-    try:
-        sql_email = do_sql(q_check_email, earl=EARL)
-        email_result = sql_email.fetchone()
-        if email_result == None:
-            print("New Email will be = " + email)
-            fnct_insert_aa(id, fullname,
-                           email, "", "", "", "", "", "",
-                           datetime.now().strftime("%Y/%m/%d"))
-            return("New email")
-        elif email_result[2] == email:
-            return("No email Change")
-        else:
-            # End date current EML2
-            print("Existing email = " + email_result[0])
-            fn_end_date_aa(id, "EML2", email_result[2],
-                        fullname,
-                        email_result[4],
-                        datetime.now().strftime("%Y-%m-%d"))
-            # insert new
-            fnct_insert_aa(id, fullname, email, "", "", "", "", "", "",
-                           datetime.now().strftime("%Y/%m/%d"))
-            print("New Email will be = " + email)
-            return("Updated email")
+# def fn_set_email2(email, id, fullname):
+#     q_check_email = '''
+#                   SELECT aa_rec.aa, aa_rec.id, aa_rec.line1,
+#                   aa_rec.aa_no, aa_rec.beg_date
+#                   FROM aa_rec
+#                   WHERE aa_rec.id = {0}
+#                   AND aa_rec.aa = 'EML2'
+#                   AND aa_rec.end_date IS NULL
+#                   '''.format(id)
+#     print(q_check_email)
+#     # logger.info("Select email info from aa_rec table");
+#     try:
+#         sql_email = do_sql(q_check_email, key=DEBUG, earl=EARL)
+#         email_result = sql_email.fetchone()
+#         if email_result == None:
+#             print("New Email will be = " + email)
+#             fnct_insert_aa(id, fullname,
+#                            email, "", "", "", "", "", "",
+#                            datetime.now().strftime("%Y/%m/%d"))
+#             return("New email")
+#         elif email_result[2] == email:
+#             return("No email Change")
+#         else:
+#             # End date current EML2
+#             print("Existing email = " + email_result[0])
+#             fn_end_date_aa(id, "EML2", email_result[2],
+#                         fullname,
+#                         email_result[4],
+#                         datetime.now().strftime("%Y-%m-%d"))
+#             # insert new
+#             fnct_insert_aa(id, fullname, email, "", "", "", "", "", "",
+#                            datetime.now().strftime("%Y/%m/%d"))
+#             print("New Email will be = " + email)
+#             return("Updated email")
+#
+#     except Exception as e:
+#         print(e)
 
-    except Exception as e:
-        print(e)
 
 
-
-#########################################################
-# Specific function to insert into aa_rec
-# Should this remain in the addresssubs.py file?
-# I think it would only be called from there
-#########################################################
-def fn_insert_aa(id, fullname, addr1, addr2, addr3, cty, st, zp, ctry, beg_date):
-    q_insert_aa = '''INSERT INTO aa_rec(id, aa, beg_date, peren, end_date,
-                         line1, line2, line3, city, st, 
-                         zip, ctry, phone, phone_ext, ofc_add_by, 
-                         cell_carrier, opt_out)
-                      VALUES
-                         ({0},{1},{2},{3},{4},
-                         {5},{6},{7},{8},{9},
-                         {10},{11},{12},{13},{14},
-                         {15},{16})
-                         '''.format(id, "PERM", beg_date, "N", "",
-                                    addr1, addr2, addr3, cty, st,
-                                    zp, ctry, "", "", "HR",
-                                    "", "")
-    #scr.write(q_insert_aa + '\n');
-    #logger.info("Inserted " + addr1 + "into aa_rec table for " + fullname + ", ID = " + id);
-    #sql_aa_insert = do_sql(q_insert_aa, earl=EARL)
-    # insert_result = sql_aa_insert.fetchone(
-    #print(q_insert_aa)
-    print("Inserted " + addr1 + "into aa_rec table for " + fullname + ", ID = " + str(id))
-
-#########################################################
-# Specific function to update aa_rec
-# Should this remain in the addresssubs.py file?
-# I think it would only be called from there
-#########################################################
-def fn_update_aa(id, aa, aanum, fullname, add1, add2, add3, cty, st, zip, ctry, begdate):
-    #print("update aa completed")
-
-    q_update_aa = '''update aa_rec 
-                      set line1 = "{4}",
-                      line2 = "{5}",
-                      line3 = "{6}",
-                      city = "{8}",
-                      st = "{9}",
-                      zip = "{10}",
-                      ctry = "{11}"  
-                      where aa_no = {2}
-                      '''.format(id, aa, aanum ,fullname, add1, add2, add3,
-                                 begdate, cty, st, zip, ctry)
-    #logger.info("Updated " + addr1 + "in aa_rec table for " + fullname + ", ID = " + id);
-    # sql_aa_insert = do_sql(q_insert_aa, earl=EARL)
-    # insert_result = sql_aa_insert.fetchone(
-    #print(q_update_aa)
-    print("Updated " + add1 + " in aa_rec table for " + fullname + ", ID = " + str(id))
-
-#########################################################
-# Specific function to end date a record in aa_rec
-# Should this remain in the addresssubs.py file?
-# I think it would only be called from there
-#########################################################
-def fn_end_date_aa(id, aa_num, addr1, fullname, begdate, enddate):
-# print("end Date aa completed")
-    q_update_aa = '''update aa_rec 
-                      set end_date = {4}
-                      where id = {0}
-                      and aa_no = {1}
-                      and beg_date = {3}
-                      '''.format(id, aa_num, fullname, begdate, enddate)
-    # logger.info("End dated " + addr1 + "in aa_rec table for " + fullname +
-    #  ", ID = " + id);
-    # sql_aa_insert = do_sql(q_insert_aa, earl=EARL)
-    # insert_result = sql_aa_insert.fetchone(
-    # print(q_update_aa)
-    print("End dated " + addr1 + " in aa_rec table for " + fullname + ", " +
-            "ID = " + str(id))
 
 
 def fn_convert_date(date):
     if date != "":
-        ndate = datetime.strptime(date, "%m-%d-%Y")
-        retdate = datetime.strftime(ndate, "%Y/%m/%d")
+        ndate = datetime.strptime(date, "%m/%d/%Y")
+        retdate = datetime.strftime(ndate, "%Y-%m-%d")
     else:
         retdate = ""
 
+    print(retdate)
     return retdate
 
 
-
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    test = args.test
-    database = args.database
-
-    if not database:
-        print "mandatory option missing: database name\n"
-        parser.print_help()
-        exit(-1)
+def fn_format_phone(phone):
+    if phone != "":
+        v =  phone[1:4]+phone[6:9]+phone[10:14]
+        return v
     else:
-        database = database.lower()
+        return ""
 
-    if database != 'cars' and database != 'train':
-        print "database must be: 'cars' or 'train'\n"
-        parser.print_help()
-        exit(-1)
 
-    sys.exit(main())
+def do_sql(sql, key, earl):
+
+    print(sql)
+    print(key)
+    print(earl)
+
+    cursor = connections[earl].cursor()
+
+    if key == "test":
+        print(sql)
+    else:
+        print(key)
+        cursor.execute(sql)
+        objects = cursor.fetchall()
+        # print(objects)
+
+        for o in objects:
+             print(o)
+
