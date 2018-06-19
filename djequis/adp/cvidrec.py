037c0f6b02dfb04d222994f4af540bbcc55b0820
dsullivan <dsullivan@carthage.edu>
2018-06-19 15:19:24 -0500

    cleaning up code and adding logging

--- djequis/adp/cvidrec.py
+++ djequis/adp/cvidrec.py
@@ -1,24 +1,21 @@
-import string
-import datetime
 import os
-import io
+import string
 import sys
 import pysftp
 import csv
 import datetime
 from datetime import date
 from datetime import datetime, timedelta
+import codecs
 import time
 from time import strftime
 import argparse
-import uuid
+#import uuid
 from sqlalchemy import text
 import shutil
-import re
+#import re
 import logging
 from logging.handlers import SMTPHandler
-#import adp_ftp
-import codecs
 
 # python path
 sys.path.append('/usr/lib/python2.7/dist-packages/')
@@ -51,13 +48,16 @@ os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
 os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
 
 # from djequis.core.utils import sendmail
-from djequis.adp.utilities import fn_validate_field, fn_check_duplicates 
 from djzbar.utils.informix import do_sql
 from djzbar.utils.informix import get_engine
 from djzbar.settings import INFORMIX_EARL_TEST
 from djzbar.settings import INFORMIX_EARL_PROD
 from djzbar.settings import INFORMIX_EARL_SANDBOX
+
 from djtools.fields import TODAY
+
+# Imports for additional modules and functions written as part of this project
+from djequis.adp.utilities import fn_validate_field, fn_check_duplicates
 
 DEBUG = settings.INFORMIX_DEBUG
 
@@ -96,83 +96,81 @@ EARL = INFORMIX_EARL_SANDBOX
 # establish database connection
 engine = get_engine(EARL)
 
+# write out the .sql file
+scr = open("apdtocx_output.sql", "a")
+# set start_time in order to see how long script takes to execute
+start_time = time.time()
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to info
+handler = logging.FileHandler('{0}apdtocx.log'.format(settings.LOG_FILEPATH))
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+# create error file handler and set level to error
+handler = logging.FileHandler('{0}apdtocx_error.log'.format(settings.LOG_FILEPATH))
+handler.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def fn_process_cvid(carthid, adpid, ssn, adp_assoc_id):
-    # create logger
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    # create console handler and set level to info
-    handler = logging.FileHandler(
-        '{0}apdtocx.log'.format(settings.LOG_FILEPATH))
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                                  datefmt='%m/%d/%Y %I:%M:%S %p')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    # create error file handler and set level to error
-    handler = logging.FileHandler(
-        '{0}apdtocx_error.log'.format(settings.LOG_FILEPATH))
-    handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                                  datefmt='%m/%d/%Y %I:%M:%S %p')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
 
     try:
         ##############################################################
         # Inserts or updates as needed into cvid_rec
         ##############################################################
 
-        # print(carthid, adpid, ssn, adp_assoc_id)
-
         # Validate the cx_id
         v_cx_id = fn_validate_field(carthid, "cx_id", "cx_id", "cvid_rec",
                     "integer")
-        print("CX_ID = " + str(carthid))
-        print("v_CX_ID = " + str(v_cx_id))
+        #print("CX_ID = " + str(carthid))
+        #print("v_CX_ID = " + str(v_cx_id))
 
         # Should also check for duplicates of the adp_id and associate_id
         # What to do in that case?
         v_adp_match = fn_check_duplicates(adpid, "adp_id", "cx_id", "cvid_rec",
                                  v_cx_id, "char")
-        print("Found_ID = " + str(v_adp_match))
+        #print("Found_ID = " + str(v_adp_match))
 
         # By definition, associate ID cannot be a duplicate in ADP, but
         # possibility of duplicate might exist in CX
         v_assoc_match = fn_check_duplicates(adp_assoc_id, "adp_associate_id",
                        "cx_id", "cvid_rec", v_cx_id, "char")
-        print("Found ID = " + str(v_assoc_match))
-
-
+        #print("Found ID = " + str(v_assoc_match))
 
         if v_cx_id == 0 and v_assoc_match == 0 and v_adp_match == 0:
             # Insert or update as needed to ID_rec
-            # Insert works 06/12/18
-
-            q_insert_cvid_rec = '''INSERT INTO cvid_rec (old_id, old_id_num, 
-              adp_id, ssn, cx_id, cx_id_char, adp_associate_id) 
+            q_insert_cvid_rec = '''
+              INSERT INTO cvid_rec (old_id, old_id_num, adp_id, ssn, cx_id, 
+              cx_id_char, adp_associate_id) 
               VALUES (?,?,?,?,?,?,?)'''
             args = (carthid, carthid, adpid, ssn, carthid, carthid, adp_assoc_id)
-
-            print(q_insert_cvid_rec)
-
+            # print(q_insert_cvid_rec)
             engine.execute(q_insert_cvid_rec, args)
-            # scr.write(q_insert_cvid_rec + '\n');
+            scr.write(q_insert_cvid_rec + '\n');
             logger.info("Inserted into cvid_rec table");
         elif str(v_cx_id) != v_assoc_match and v_assoc_match != 0:
             print('Duplicate Associate ID found')
         elif str(v_cx_id) != str(v_adp_match) and v_adp_match != 0:
             print('Duplicate ADP ID found')
         else:
-            # sql works - 6/12/18
-            q_update_cvid_rec = '''UPDATE cvid_rec SET old_id = ?, 
-                old_id_num = ?, adp_id = ?, ssn = ?, adp_associate_id = ? 
-                WHERE cx_id = ?'''
+            q_update_cvid_rec = '''
+              UPDATE cvid_rec 
+              SET old_id = ?, old_id_num = ?, adp_id = ?, ssn = ?, 
+              adp_associate_id = ? 
+              WHERE cx_id = ?'''
             args = (carthid, carthid, adpid, ssn, adp_assoc_id, carthid)
-            print(q_update_cvid_rec)
+            # print(q_update_cvid_rec)
             logger.info("Update cvid_rec table");
+            scr.write(q_update_cvid_rec + '\n');
             engine.execute(q_update_cvid_rec, args)
 
     except Exception as e:
         print(e)
+    finally:
+        logging.shutdown()
