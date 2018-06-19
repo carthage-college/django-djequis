96f2c9c71d232e4c3f091473c4be8df888e171ad
dsullivan <dsullivan@carthage.edu>
2018-06-19 15:21:52 -0500

    cleaning up code and adding logging

--- djequis/adp/profilerec.py
+++ djequis/adp/profilerec.py
@@ -1,24 +1,21 @@
-import string
-import datetime
 import os
-import io
+import string
 import sys
 import pysftp
 import csv
 import datetime
 from datetime import date
 from datetime import datetime, timedelta
+import codecs
 import time
 from time import strftime
 import argparse
-import uuid
+#import uuid
 from sqlalchemy import text
 import shutil
-import re
+#import re
 import logging
 from logging.handlers import SMTPHandler
-#import adp_ftp
-import codecs
 
 # python path
 sys.path.append('/usr/lib/python2.7/dist-packages/')
@@ -51,13 +48,16 @@ os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
 os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
 
 # from djequis.core.utils import sendmail
-from djequis.adp.utilities import fn_validate_field, fn_convert_date
 from djzbar.utils.informix import do_sql
 from djzbar.utils.informix import get_engine
 from djzbar.settings import INFORMIX_EARL_TEST
 from djzbar.settings import INFORMIX_EARL_PROD
 from djzbar.settings import INFORMIX_EARL_SANDBOX
+
 from djtools.fields import TODAY
+
+# Imports for additional modules and functions written as part of this project
+from djequis.adp.utilities import fn_validate_field, fn_convert_date, fn_calculate_age
 
 DEBUG = settings.INFORMIX_DEBUG
 
@@ -96,39 +96,38 @@ EARL = INFORMIX_EARL_SANDBOX
 # establish database connection
 engine = get_engine(EARL)
 
+# write out the .sql file
+scr = open("apdtocx_output.sql", "a")
+# set start_time in order to see how long script takes to execute
+start_time = time.time()
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to info
+handler = logging.FileHandler('{0}apdtocx.log'.format(settings.LOG_FILEPATH))
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+# create error file handler and set level to error
+handler = logging.FileHandler('{0}apdtocx_error.log'.format(settings.LOG_FILEPATH))
+handler.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
+                              datefmt='%m/%d/%Y %I:%M:%S %p')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
         prof_last_upd_date):
-
-
-    # create logger
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    # create console handler and set level to info
-    handler = logging.FileHandler(
-        '{0}apdtocx.log'.format(settings.LOG_FILEPATH))
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                                  datefmt='%m/%d/%Y %I:%M:%S %p')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    # create error file handler and set level to error
-    handler = logging.FileHandler(
-        '{0}apdtocx_error.log'.format(settings.LOG_FILEPATH))
-    handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                                  datefmt='%m/%d/%Y %I:%M:%S %p')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
     try:
         ##########################################################
         #  Find out if record exists to determine update vs insert
         ##########################################################
         prof_rslt = fn_validate_field(id, "id", "id",
                                       "profile_rec", "integer")
-        print("Prof Result = " + str(prof_rslt))
-
-        # create ethnicity dictionary
+        #print("Prof Result = " + str(prof_rslt))
+        # create race dictionary
         racecode = {
             '1': 'WH',
             '2': 'BL',
@@ -137,30 +136,29 @@ def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
             '9': 'MU'
         }
         race = racecode.get(race)
+        # create ethnicity dictionary
         ethnic_code = {
             'Not Hispanic or Latino': 'N',
             'HISPANIC OR LATINO': 'Y'
         }
         is_hispanic = ethnic_code.get(ethnicity)
-        print(is_hispanic)
-
+        #print(is_hispanic)
         age = fn_calculate_age(birth_date)
-        print("Age = " + str(age))
-
+        #print("Age = " + str(age))
         if prof_rslt is None or prof_rslt == 0:
             # Insert or update as needed
             q_insert_prof_rec = '''
-                       INSERT INTO profile_rec (id, sex, 
-                           race, hispanic, birth_date, age, prof_last_upd_date)
-                           VALUES (?, ?, ?, ?, ?, ?, ?) '''
-            q_ins_prof_args=(id, sex, race, is_hispanic,
-                birth_date, age, prof_last_upd_date)
+              INSERT INTO profile_rec (id, sex, race, hispanic, birth_date, 
+                age, prof_last_upd_date)
+              VALUES (?, ?, ?, ?, ?, ?, ?) '''
+            q_ins_prof_args=(id, sex, race, is_hispanic, birth_date, age,
+                             prof_last_upd_date)
             # print(q_insert_prof_rec)
             # print(q_ins_prof_args)
             engine.execute(q_insert_prof_rec, q_ins_prof_args)
             # scr.write(q_insert_prof_rec + '\n');
             logger.info("Inserted into profile_rec table");
-
+            scr.write(q_insert_prof_rec + '\n');
         else:
             q_update_prof_rec = '''
                        UPDATE profile_rec SET sex = ?,
@@ -178,16 +176,5 @@ def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
 
     except Exception as e:
         print(e)
-
-
-
-def fn_calculate_age(bdate):
-    # print("Birtdate = " + bdate)
-    d_born = datetime.strptime(bdate, '%m/%d/%Y')
-
-    # print(d_born)
-    today = date.today()
-    # print(today)
-    age = today.year - d_born.year - ((today.month, today.day) < (d_born.month, d_born.day))
-    # print(age)
-    return(age)
+    finally:
+        logging.shutdown()
