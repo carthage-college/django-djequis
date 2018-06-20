0b134ffcc3af52bab3b99097ab89c6b66636b4c5
dsullivan <dsullivan@carthage.edu>
2018-06-20 16:10:21 -0500

    set to pass setting params to modules to reduce redundant code

--- djequis/adp/profilerec.py
+++ djequis/adp/profilerec.py
@@ -57,7 +57,8 @@ from djzbar.settings import INFORMIX_EARL_SANDBOX
 from djtools.fields import TODAY
 
 # Imports for additional modules and functions written as part of this project
-from djequis.adp.utilities import fn_validate_field, fn_convert_date, fn_calculate_age
+from djequis.adp.utilities import fn_validate_field, fn_convert_date, \
+    fn_calculate_age, fn_write_error, fn_write_log
 
 DEBUG = settings.INFORMIX_DEBUG
 
@@ -65,61 +66,44 @@ DEBUG = settings.INFORMIX_DEBUG
 desc = """
     Upload ADP data to CX
 """
-parser = argparse.ArgumentParser(description=desc)
-
-parser.add_argument(
-    "--test",
-    action='store_true',
-    help="Dry run?",
-    dest="test"
-)
-parser.add_argument(
-    "-d", "--database",
-    help="database name.",
-    dest="database"
-)
-
-# set global variable
-global EARL
-# determines which database is being called from the command line
-# if database == 'cars':
-#    EARL = INFORMIX_EARL_PROD
-# elif database == 'train':
-# EARL = INFORMIX_EARL_TEST
-# elif database = 'sandbox'
-EARL = INFORMIX_EARL_SANDBOX
-# else:
-    # this will raise an error when we call get_engine()
-    # below but the argument parser should have taken
-    # care of this scenario and we will never arrive here.
-#    EARL = None
-# establish database connection
-engine = get_engine(EARL)
+# parser = argparse.ArgumentParser(description=desc)
+#
+# parser.add_argument(
+#     "--test",
+#     action='store_true',
+#     help="Dry run?",
+#     dest="test"
+# )
+# parser.add_argument(
+#     "-d", "--database",
+#     help="database name.",
+#     dest="database"
+# )
+#
+# # set global variable
+# global EARL
+# # determines which database is being called from the command line
+# # if database == 'cars':
+# #    EARL = INFORMIX_EARL_PROD
+# # elif database == 'train':
+# # EARL = INFORMIX_EARL_TEST
+# # elif database = 'sandbox'
+# EARL = INFORMIX_EARL_SANDBOX
+# # else:
+#     # this will raise an error when we call get_engine()
+#     # below but the argument parser should have taken
+#     # care of this scenario and we will never arrive here.
+# #    EARL = None
+# # establish database connection
+# engine = get_engine(EARL)
 
 # write out the .sql file
 scr = open("apdtocx_output.sql", "a")
-# set start_time in order to see how long script takes to execute
-start_time = time.time()
-# create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-# create console handler and set level to info
-handler = logging.FileHandler('{0}apdtocx.log'.format(settings.LOG_FILEPATH))
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                              datefmt='%m/%d/%Y %I:%M:%S %p')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-# create error file handler and set level to error
-handler = logging.FileHandler('{0}apdtocx_error.log'.format(settings.LOG_FILEPATH))
-handler.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                              datefmt='%m/%d/%Y %I:%M:%S %p')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
-        prof_last_upd_date):
+        prof_last_upd_date, EARL):
+    engine = get_engine(EARL)
+
     try:
         ##########################################################
         #  Find out if record exists to determine update vs insert
@@ -157,7 +141,7 @@ def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
             # print(q_ins_prof_args)
             engine.execute(q_insert_prof_rec, q_ins_prof_args)
             # scr.write(q_insert_prof_rec + '\n');
-            logger.info("Inserted into profile_rec table");
+            fn_write_log("Inserted into profile_rec table");
             scr.write(q_insert_prof_rec + '\n');
         else:
             q_update_prof_rec = '''
@@ -168,13 +152,14 @@ def fn_process_profile_rec(id, ethnicity, sex, race, birth_date,
                            WHERE id = ?'''
             q_upd_prof_args = (sex, is_hispanic, race,
                 birth_date, age, prof_last_upd_date, id)
-            print(q_update_prof_rec)
-            print(q_upd_prof_args)
+            # print(q_update_prof_rec)
+            # print(q_upd_prof_args)
             engine.execute(q_update_prof_rec, q_upd_prof_args)
             # scr.write(q_update_prof_rec + '\n');
-            logger.info("Update profile_rec table");
+            fn_write_log("Update profile_rec table");
 
     except Exception as e:
         print(e)
-    finally:
-        logging.shutdown()
+        fn_write_error(e)
+    # finally:
+    #     logging.shutdown()
