3f0cc1567470988ca8598bf6236ada47bc2672ee
dsullivan <dsullivan@carthage.edu>
2018-07-13 11:17:16 -0500

    removed unneeded header code

--- djequis/adp/aarec.py
+++ djequis/adp/aarec.py
@@ -1,60 +1,13 @@
-import os
-import string
-import sys
-import pysftp
-import csv
 import datetime
 from datetime import date
 from datetime import datetime, timedelta
-import codecs
 import time
-from time import strftime
-import argparse
-#import uuid
-from sqlalchemy import text
-import shutil
-#import re
-# import logging
-# from logging.handlers import SMTPHandler
-
-# python path
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_1.11/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
-sys.path.append('/djequis/adp')
-
-# django settings for shell environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
-
-# prime django
-import django
-django.setup()
 
 # django settings for script
 from django.conf import settings
-from django.db import connections
 
-# informix environment
-os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
-os.environ['DBSERVERNAME'] = settings.DBSERVERNAME
-os.environ['INFORMIXDIR'] = settings.INFORMIXDIR
-os.environ['ODBCINI'] = settings.ODBCINI
-os.environ['ONCONFIG'] = settings.ONCONFIG
-os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
-os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
-os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
-
-# from djequis.core.utils import sendmail
 from djzbar.utils.informix import do_sql
 from djzbar.utils.informix import get_engine, get_session
-from djzbar.settings import INFORMIX_EARL_TEST
-from djzbar.settings import INFORMIX_EARL_PROD
-from djzbar.settings import INFORMIX_EARL_SANDBOX
-
-from djtools.fields import TODAY
 
 # Imports for additional modules and functions written as part of this project
 from djequis.adp.utilities import fn_convert_date, fn_write_log, \
