e232bfdb5782fedf7a23ff73814b83a9988a9181
ssmolik <ssmolik@carthage.edu>
2018-08-14 14:28:56 -0500

    updated SQL query the opt_out field needed to be a CASE statement not a WHERE clause

--- djequis/sql/concierge.py
+++ djequis/sql/concierge.py
@@ -3,38 +3,27 @@
 # The Y and N are somewhat counter-intuitive in that N means we can text them and Y means we cannot.
 # Privacy - students who have FERP are excluded from the list
 STUDENTS = '''
-    SELECT DISTINCT DIR.id AS Student_ID, DIR.firstname AS First_Name, DIR.lastname AS Last_Name, DIR.email AS Email_Address,
-    TRIM(NVL(CELL.phone,'')) AS Cell_Phone, TRIM(DIR.addr_line1) AS Address,
-    TRIM(DIR.city) AS City, TRIM(DIR.st) AS State,
-    TRIM(DIR.zip) AS Zip, HSG.yr AS Year, HSG.sess AS Session, HSG.intend_hsg AS Housing,
-    CASE NVL(HSG.bldg,'')
-        WHEN 'CMTR'  THEN 'Commuter'
-        WHEN 'OFF'   THEN 'Off Campus'
-                     ELSE TRIM(NVL(BLDG,''))
-    END AS Building,
-    CASE
-        WHEN NVL(HSG.room,'') LIKE 'UN%' THEN ''
-                                         ELSE NVL(HSG.room,'')
-    END AS Room_Number, CELL.opt_out AS OPT_OUT, PROF.priv_code AS Privacy
+    SELECT DISTINCT 
+        DIR.id AS studentid, DIR.firstname AS firstname, DIR.lastname AS lastname, 
+        DIR.email AS emailaddress, '2001 Alford Park Drive' AS address, 'Kenosha' AS city, 
+        'WI' AS state, '53140' AS zip
     FROM directory_vw DIR
-    INNER JOIN stu_serv_rec HSG ON DIR.id = HSG.id
-    INNER JOIN profile_rec PROF ON DIR.id = PROF.id
-    INNER JOIN acad_cal_rec ACR
-    on HSG.sess = ACR.sess
-    and HSG.yr = ACR.yr
-    and ACR.acyr = CASE WHEN TODAY < TO_DATE(YEAR(TODAY) || '-07-01', '%Y-%m-%d')	THEN MOD(YEAR(TODAY) - 1, 100) || MOD(YEAR(TODAY), 100)
-                                                                                    ELSE MOD(YEAR(TODAY), 100) || MOD(YEAR(TODAY) + 1, 100)
-                    END
-    and ACR.beg_date >= TODAY
-    and ACR.end_date < ADD_MONTHS(today,6)
-    LEFT JOIN aa_rec CELL ON DIR.id = CELL.id
-        AND CELL.aa = 'CELL'
-        AND TODAY BETWEEN CELL.beg_date
-        AND NVL(CELL.end_date, TODAY)
-    where HSG.yr = YEAR(TODAY)
-    AND HSG.intend_hsg = 'R'
-    AND CELL.opt_out != 'Y'
-    AND PROF.priv_code != 'FERP'
+        INNER JOIN stu_serv_rec SSR ON DIR.id = SSR.id
+        INNER JOIN profile_rec PROF ON DIR.id = PROF.id
+        INNER JOIN acad_cal_rec ACR ON SSR.sess = ACR.sess
+        AND SSR.yr = ACR.yr
+        AND ACR.acyr = CASE WHEN TODAY < TO_DATE(YEAR(TODAY) || '-07-01', '%Y-%m-%d')   THEN MOD(YEAR(TODAY) - 1, 100) || MOD(YEAR(TODAY), 100)
+                                                                                        ELSE MOD(YEAR(TODAY), 100) || MOD(YEAR(TODAY) + 1, 100)
+                        END
+        AND ACR.beg_date >= TODAY
+        AND ACR.end_date < ADD_MONTHS(today,6)
+        LEFT JOIN aa_rec ON DIR.id = aa_rec.id
+            AND aa_rec.aa = 'CELL'
+            AND TODAY BETWEEN aa_rec.beg_date
+            AND NVL(aa_rec.end_date, TODAY)
+        WHERE SSR.yr = YEAR(TODAY)
+        AND SSR.intend_hsg = 'R'
+        AND PROF.priv_code != 'FERP'
     ORDER BY
         DIR.lastname, DIR.firstname
 '''
