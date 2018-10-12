aa3be7b8b798070062648ec001a875f1788cbfd9
dsullivan <dsullivan@carthage.edu>
2018-10-12 15:31:09 -0500

    Add variable passing option

--- djequis/core/trustcommerce/urls.py
+++ djequis/core/trustcommerce/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 
 from djequis.core.trustcommerce import views
-
+from djequis.core.trustcommerce.views import home, details, index
 
 urlpatterns = [
     # home
-    url(
-        r'^$',
-        views.home, name='trustcommerce_home'
-    )
+    url(r'^$', views.home, name='trustcommerce_home'),
+    # Details
+    # Details
+    url(r'^(?P<activity>\d+) details$', views.details, name='trustcommerce_details')
+    # url(r'^details$', views.details, name='trustcommerce_details')
+
 ]
+
