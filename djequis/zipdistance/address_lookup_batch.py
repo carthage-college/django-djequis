17ffab6e6f8167803ae06eeaf31a609de73067fa
dsullivan <dsullivan@carthage.edu>
2018-10-23 08:55:34 -0500

    Added update sql for profile record and trapped for empty  last line of census file

--- djequis/zipdistance/address_lookup_batch.py
+++ djequis/zipdistance/address_lookup_batch.py
@@ -111,112 +111,133 @@ def main():
         #-------------------------------------------------------
         # Read in records first.
         #-------------------------------------------------------
-        # searchval = "Winthrop Harbor"
-        #
-        # qval_sql = "select id, fullname, addr_line1, addr_line2, addr_line3, " \
-        #            "city, st, zip from id_rec where city = '" \
-        #            + str(searchval) + "'"
-        #
-        # sql_val = do_sql(qval_sql, key=DEBUG, earl=EARL)
-        #
-        # if sql_val is not None:
-        #     rows = sql_val.fetchall()
-        #
-        #     for row in rows:
-        #         # response2 = requests.get("http://www.zipcodeapi.com/rest/" + APIKey + "/"
-        #         # + distformat + "/" + CarthZip + "/" + zipcode + "/" + distunits)
-        #         v_street = row[2]
-        #         v_city = row[5]
-        #         v_state = row[6]
-        #         v_zip = row[7]
 
-        # Write the query results to a csv file that can be uploaded as batch
-        # The first item is unique id, so we need to use the Carthage ID
-        # so that we will have it on the return...
+        if os.path.exists("Addresses1.csv"):
+            os.remove("Addresses1.csv")
+        else:
+            print("The file does not exist")
+
+        searchval = '190500'
+
+        qval_sql = "select id, fullname, addr_line1, addr_line2, addr_line3, " \
+                   "city, st, zip from id_rec where id = '" \
+                   + searchval + "'"
+
+        sql_val = do_sql(qval_sql, key=DEBUG, earl=EARL)
+
+        endline = ''
+        if sql_val is not None:
+            rows = sql_val.fetchall()
+
+            for row_no, row in enumerate(rows):
+                x = len(row)
+                v_id = row[0]
+                v_street = row[2] + row[3] + row[4]
+                v_city = row[5]
+                v_state = row[6]
+                v_zip = row[7]
+
+                line = str(v_id) + "," + v_street + "," + v_city + "," + v_state + "," + v_zip
+                # print("Line = " + line)
+                # print("ID = " + str(v_id) + "|")
+                # print("Row Number = " + str(row_no + 1))
+
+                # -------------------------------------------------------
+                # Write the query results to a csv file that can be uploaded as batch
+                # The first item is unique id, so we need to use the Carthage ID
+                # so that we will have it on the return...
+                # -------------------------------------------------------
+                if str(v_id) == "":
+                    print("Blank line")
+                else:
+                    f = open('Addresses1.csv', 'a')
+                    f.write(endline + line )
+                    f.close()
+                    endline = '\n'
 
 
 
-
-
-
-        # This seems to be the correct method, but it just returns a massive xml page
+        # -------------------------------------------------------
+        # Send the CSV via the API to collect the geographic data
+        # -------------------------------------------------------
         url = 'https://geocoding.geo.census.gov/geocoder/geographies/addressbatch?form'
         payload = {'benchmark': 'Public_AR_Current',
                    'vintage': 'Current_Current'}
-        files = {'addressFile': ('Addresses.csv', open('Addresses.csv', 'rb'), 'text/csv')}
+        files = {'addressFile': ('Addresses1.csv', open('Addresses1.csv', 'rb'), 'text/csv')}
         r = requests.post(url, files=files, data=payload)
-        # just a 404 error
-        print r.status_code
+
         results = str(r.text)
         results = results.replace('"', '')
         results = results.split('\n')
-        print(results)
+        # print(results)
         with open('geocodeOutput.csv', 'w') as geocodeOutput:
             w = csv.writer(geocodeOutput, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             w.writerows([c.strip() for c in r.split(',')] for r in results)
 
         with open('geocodeOutput.csv', 'r') as data:
             read_csv = csv.reader(data, delimiter=',')
-            for row in read_csv:
-                print("ID = " + row[0])
-                print("Street = " + row[1])
-                print("City = " + row[2])
-                print("State = " + row[3])
-                print("Zip = " + row[4])
-                print("Match = " + row[5])
-                print("Match Type = " + row[6])
-                print("Corrected Street = " + row[7])
-                print("Corrected City = " + row[8])
-                print("Corrected State = " + row[9])
-                print("Zip = " + row[10])
-                print("Longitude = " + row[11])
-                print("Latitude = " + row[12])
-                print("Side = " + row[13])
-                print("FIPS State = " + row[14])
-                print("FIPS County = " + row[15])
+            for row_count, row in enumerate(read_csv):
+                # print("Row count = " + str(row_count + 1))
+                if row[0] == '':
+                    print("NO Record")
+                elif row[5] == 'No_Match':
+                    print("No Match")
+                else:
+                    print("ID = " + row[0])
+                    print("Match = " + row[5])
+                    print("Match Type = " + row[6])
+                    original_address = row[1] + ", " + row[2] + ", " + row[3] + ", " + row[4]
+                    print(original_address)
+                    correct_address = row[7] + ", " + row[8] + ", " + row[9] + ", " + row[10]
+                    print(correct_address)
+                    coordinates = str(row[12] + "," + str(row[11]))
+                    print(coordinates)
+                    FIPS = str(row[15]) + "-" + str(row[16])
+                    print("FIPS = " + FIPS)
 
-                # Return file is also a csv, so no JSON
-                # Loop through csv, update tables as needed
+                    # Return file is also a csv, so no JSON
+                    # Loop through csv, update tables as needed
 
-                y_coordinate = row[12]
-                x_coordinate = row[11]
+                    y_coordinate = row[12]
+                    x_coordinate = row[11]
 
-                # print("Coordinates = " + str(x_coordinate) + ", " + str(y_coordinate))
+                    # print("Coordinates = " + str(x_coordinate) + ", " + str(y_coordinate))
 
-                # Calculate distance using latitude and longitude
-                # Note radians must be converted to a positive number
-                # Carthage latitude and longitude
-                radius_earth = 3958.756
-                lat1 = radians(42.62233)
-                lng1 = radians(abs(-87.828699))
-                lat2 = radians(float(y_coordinate))
-                lng2 = radians(abs(float(x_coordinate)))
+                    # Calculate distance using latitude and longitude
+                    # Note radians must be converted to a positive number
+                    # Carthage latitude and longitude
+                    radius_earth = 3958.756
+                    lat1 = radians(42.62233)
+                    lng1 = radians(abs(-87.828699))
+                    lat2 = radians(float(y_coordinate))
+                    lng2 = radians(abs(float(x_coordinate)))
 
-                dlon = lng2 - lng1
-                dlat = lat2 - lat1
+                    dlon = lng2 - lng1
+                    dlat = lat2 - lat1
 
-                a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-                c = 2 * atan2(sqrt(a), sqrt(1 - a))
+                    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+                    c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-                distance = radius_earth * c
-                # print("Result rounded = " + "{:.0f}".format(distance))
-                dist = float("{:.2f}".format(distance))
+                    distance = radius_earth * c
+                    # print("Result rounded = " + "{:.0f}".format(distance))
+                    dist = float("{:.2f}".format(distance))
 
-                print("Distance from Carthage = " + str(dist))
+                    print("Distance from Carthage = " + str(dist))
 
-
-                # Finally, update the particular CX tables
-                # Update profile rec set ...
-                # This creates a new problem in that the return file is no
-                # longer associated to a person's ID number
-                # How then do we make the match?
+                    # -------------------------------------------------------
+                    # Finally, update the particular CX tables
+                    # -------------------------------------------------------
+                    profile_sql = "UPDATE profile_rec SET res_st = ?, res_cty = ? WHERE id = ?"
+                    profile_args = (str(row[15]), str(row[16]), row[0])
+                    print(profile_sql, profile_args)
+                    engine.execute(profile_sql, profile_args)
 
 
 
 
     except Exception as e:
         # fn_write_error("Error in zip_distance.py for zip, Error = " + e.message)
-        print(e.message)
+        print("Error = " + e.message)
         # finally:
         #     logging.shutdown()
 
