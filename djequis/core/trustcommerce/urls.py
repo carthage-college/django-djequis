df07dfbeb285d1b71fa82a7390f844deb8fd6ea9
dsullivan <dsullivan@carthage.edu>
2018-10-26 15:33:40 -0500

    attempt to add third url pattern

--- djequis/core/trustcommerce/urls.py
+++ djequis/core/trustcommerce/urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls import url
-
 from djequis.core.trustcommerce import views
-from djequis.core.trustcommerce.views import home, details, index
+from djequis.core.trustcommerce.views import home, details, index, download
 
+app_name = 'trust_commerce'
 urlpatterns = [
+
+    # Download CSV
+    url(r'^$', views.download, name='download'),
+
     # home
     url(r'^$', views.home, name='trustcommerce_home'),
+
     # Details
-    # Details
-    url(r'^(?P<activity>\d+) details$', views.details, name='trustcommerce_details')
-    # url(r'^details$', views.details, name='trustcommerce_details')
+    url(r'^(?P<activity>\d+) details$', views.details, name='trustcommerce_details'),
+
 
 ]
 
