c728eee88d1fd118c4d1b03af43bf6c1fc72d169
dsullivan <dsullivan@carthage.edu>
2018-10-30 15:50:47 -0500

    added third sql for step 3

--- djequis/core/trustcommerce/sql.py
+++ djequis/core/trustcommerce/sql.py
@@ -48,8 +48,7 @@ PCE_TRANSACTIONS_ACTIVITY = '''
         transactions.pnref,
         activities.webtranscode  as SelWTCode,
         "tcpayflow" as datasource
-    FROM
-        transactions, activities
+    FROM `transactions`, `activities`
     WHERE
         transactions.activityID = activities.id
     AND
@@ -76,7 +75,7 @@ PSM_TRANSACTIONS_ACTIVITY = '''
         processors_activity.webtranscode as SelWTCode,
         "djforms_on_psm" as datasource
     FROM
-        processors_order, processors_activity
+        `processors_order`, `processors_activity`
     WHERE
        processors_activity.name = processors_order.operator
     AND
@@ -98,7 +97,6 @@ PCE_TRANSACTIONS_CHECKED = '''
         activities.ID as activityID,
         transactions.id as RecID,
         concat(cast(left(activities.orderacct,7) as char(7)), cast(right(activities.orderacct,4) as char(4))) as acct,
-        transactions.activityID,
         transactions.amt,
         transactions.comment1 as comment1,
         transactions.comment2 as StuID,
@@ -108,40 +106,46 @@ PCE_TRANSACTIONS_CHECKED = '''
         transactions.pnref,
         activities.webtranscode as webtranscode,
         activities.subsID as subsID,
+        activities.webtranscode as SelWTCode,
+        '' as ccnum,
         "tcpayflow" as datasource
     FROM
         transactions, activities
     WHERE
         transactions.activityID = activities.id
     AND
-        transactions.id in ({CheckedVal})
+        transactions.id in ({0})
+    AND
+        activities.ID = ({1})
 '''
 
 
 PSM_TRANSACTIONS_CHECKED = '''
     SELECT
         processors_order.operator as activity,
-        processors_order.id as RecID,
         processors_activity.ID as activityID,
+        processors_order.id as RecID,
+        concat(cast(left(processors_activity.orderacct,7) as char(7)), cast(right(processors_activity.orderacct,4) as char(4))) as acct,
         processors_order.total as amt,
         processors_order.comments as comment1,
-        concat(cast(left(processors_activity.orderacct,7) as char(7)), cast(right(processors_activity.orderacct,4) as char(4))) as acct,
         "Comment2" as StuID,
         processors_order.time_stamp as datecreated,
         processors_order.cc_name as name,
-        processors_order.cc_4_digits as ccnum,
         processors_order.status as result,
         processors_order.transid as pnref,
+        processors_activity.webtranscode as webtranscode,
         processors_activity.subsid as subsID,
         processors_activity.webtranscode as SelWTCode,
-        processors_activity.webtranscode as webtranscode,
+        processors_order.cc_4_digits as ccnum,
         "djforms_on_psm" as datasource
     FROM
         processors_order, processors_activity
     WHERE
        processors_activity.name = processors_order.operator
     AND
-      processors_order.id in ({CheckedVal})
+      processors_order.id in ({0})
+    AND
+      processors_activity.ID = ({1})
 '''
 
 
