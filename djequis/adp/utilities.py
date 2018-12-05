c964a4f8b0a000f4e2a4b214f8de1b2b733a7358
dsullivan <dsullivan@carthage.edu>
2018-12-05 15:49:38 -0600

    added trap for truncated division code

--- djequis/adp/utilities.py
+++ djequis/adp/utilities.py
@@ -1,61 +1,17 @@
-import os
-import string
-import sys
-import pysftp
-import csv
 import datetime
 from datetime import date
 from datetime import datetime, timedelta
-import codecs
 import time
 from time import strftime
-import argparse
-#import uuid
-from sqlalchemy import text
-import shutil
-#import re
 import logging
 from logging.handlers import SMTPHandler
 
-
-# python path
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_1.11/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
-sys.path.append('/djequis/adp')
-
-# django settings for shell environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
-
-# prime django
-import django
-django.setup()
-
-# # django settings for script
+# django settings for script
 from django.conf import settings
-from django.db import connections
-
-# informix environment
-os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
-os.environ['DBSERVERNAME'] = settings.DBSERVERNAME
-os.environ['INFORMIXDIR'] = settings.INFORMIXDIR
-os.environ['ODBCINI'] = settings.ODBCINI
-os.environ['ONCONFIG'] = settings.ONCONFIG
-os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
-os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
-os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
 
 from djequis.core.utils import sendmail
 from djzbar.utils.informix import do_sql
 from djzbar.utils.informix import get_engine
-from djzbar.settings import INFORMIX_EARL_TEST
-from djzbar.settings import INFORMIX_EARL_PROD
-from djzbar.settings import INFORMIX_EARL_SANDBOX
-
-from djtools.fields import TODAY
 
 DEBUG = settings.INFORMIX_DEBUG
 
@@ -118,7 +74,7 @@ def fn_validate_field(searchval, keyfield, retfield, table, keytype, EARL):
 # Common function to validate that a record and description exists
 # and decide based on description whether to update
 #########################################################
-def fn_needs_upate(searchval, descr_val, keyfield, descr_field,
+def fn_needs_update(searchval, descr_val, keyfield, descr_field,
                       table, keytype, EARL):
     if keytype == "char":
         qval_sql = "SELECT DISTINCT " + keyfield + "," + descr_field \
