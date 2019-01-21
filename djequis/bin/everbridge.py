87a5332aec3b8ef8ab14a3b4c4d5de9bfe4bd377
plungerman <plungerman@gmail.com>
2019-01-21 14:14:07 -0600

    prevent file generation and sftp if no results from database

--- djequis/bin/everbridge.py
+++ djequis/bin/everbridge.py
@@ -100,111 +100,112 @@ def main():
        'private_key':settings.EVERBRIDGE_PKEY,
        'cnopts':cnopts
     }
-    badmatches = []
     for key, value in dict.iteritems():
         if limit:
             value += "LIMIT {}".format(limit)
         if test:
             print("key = {}, value = {}".format(key, value))
+
+        badmatches = []
         sqlresult = engine.execute(value)
-        datetimestr = time.strftime("%Y%m%d%H%M%S")
-        filename=('{}{}Upload-{}.csv'.format(
-            settings.EVERBRIDGE_CSV_OUTPUT,key,datetimestr
-        ))
 
-        #phile=io.open(filename, 'w', newline='')
-        phile=open(filename, 'wb')
-        output=csv.writer(phile, dialect='excel', lineterminator='\n')
+        if sqlresult:
+            datetimestr = time.strftime("%Y%m%d%H%M%S")
+            filename=('{}{}Upload-{}.csv'.format(
+                settings.EVERBRIDGE_CSV_OUTPUT,key,datetimestr
+            ))
 
-        if key == 'FacStaff': # write header row for FacStaff
-            output.writerow([
-                "First Name","Middle Initial","Last Name","Suffix",
-                "External ID","Country","Business Name","Record Type",
-                "Phone 1","Phone Country 1","Phone 2","Phone Country 2",
-                "Email Address 1","Email Address 2","SMS 1","SMS 1 Country",
-                "Custom Field 1","Custom Value 1","Custom Field 2",
-                "Custom Value 2","Custom Field 3","Custom Value 3","END"
-            ])
-        else: # write header row for Student and Adult
-            output.writerow([
-                "First Name","Middle Initial","Last Name","Suffix",
-                "External ID","Country","Business Name","Record Type",
-                "Phone 1","Phone Country 1","Email Address 1",
-                "Email Address 2","SMS 1","SMS 1 Country","Custom Field 1",
-                "Custom Value 1","Custom Field 2","Custom Value 2",
-                "Custom Field 3","Custom Value 3","END"
-            ])
-        # should we parse out all of the data and then
-        # exclude Bad match data?
-        for row in sqlresult:
-            output.writerow(row)
-            if test:
-                print("row = \n{}".format(row))
-            # checking for Bad match in either Student or FacStaff query
-            if row and ((row.customvalue1 and "Bad match:" in row.customvalue1)\
-              or (row.customvalue2 and "Bad match:" in row.customvalue2)):
-                badmatches.append(
-                    '{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, '
-                    '{10} {11} {12}, {13}, {14}, {15}, {16}, {17}, {18}, '
-                    '{19} {20}\n\n'.format(
-                        row.lastname, row.firstname, row.middleinitial,
-                        row.suffix, row.externalid, row.country,
-                        row.businessname, row.recordtype,
-                        row.phone1, row.phonecountry1, row.emailaddress1,
-                        row.emailaddress2, row.sms1, row.sms1country,
-                        row.customfield1, row.customvalue1, row.customfield2,
-                        row.customvalue2, row.customfield3, row.customvalue3,
-                        row.end
+            #phile=io.open(filename, 'w', newline='')
+            phile=open(filename, 'wb')
+            output=csv.writer(phile, dialect='excel', lineterminator='\n')
+
+            if key == 'FacStaff': # write header row for FacStaff
+                output.writerow([
+                    "First Name","Middle Initial","Last Name","Suffix",
+                    "External ID","Country","Business Name","Record Type",
+                    "Phone 1","Phone Country 1","Phone 2","Phone Country 2",
+                    "Email Address 1","Email Address 2","SMS 1","SMS 1 Country",
+                    "Custom Field 1","Custom Value 1","Custom Field 2",
+                    "Custom Value 2","Custom Field 3","Custom Value 3","END"
+                ])
+            else: # write header row for Student and Adult
+                output.writerow([
+                    "First Name","Middle Initial","Last Name","Suffix",
+                    "External ID","Country","Business Name","Record Type",
+                    "Phone 1","Phone Country 1","Email Address 1",
+                    "Email Address 2","SMS 1","SMS 1 Country","Custom Field 1",
+                    "Custom Value 1","Custom Field 2","Custom Value 2",
+                    "Custom Field 3","Custom Value 3","END"
+                ])
+            for row in sqlresult:
+                output.writerow(row)
+                if test:
+                    print("row = \n{}".format(row))
+                # checking for Bad match in either Student or FacStaff query
+                if row and ((row.customvalue1 and "Bad match:" in row.customvalue1)\
+                or (row.customvalue2 and "Bad match:" in row.customvalue2)):
+                    badmatches.append(
+                        '{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, '
+                        '{10} {11} {12}, {13}, {14}, {15}, {16}, {17}, {18}, '
+                        '{19} {20}\n\n'.format(
+                            row.lastname, row.firstname, row.middleinitial,
+                            row.suffix, row.externalid, row.country,
+                            row.businessname, row.recordtype,
+                            row.phone1, row.phonecountry1, row.emailaddress1,
+                            row.emailaddress2, row.sms1, row.sms1country,
+                            row.customfield1, row.customvalue1, row.customfield2,
+                            row.customvalue2, row.customfield3, row.customvalue3,
+                            row.end
+                        )
                     )
-                )
-            badmatches_table = ''.join(badmatches)
-            if test:
-                print("badmatches = \n{}".format(badmatches))
-        if badmatches:
-            if test:
-                print("badmatches_table = \n{}".format(badmatches_table))
-                print("length of badmatches = {}.".format(len(badmatches)))
-            SUBJECT = '[Everbridge] Bad match'
-            BODY = '''
-                A bad match exists in the file we are sending to Everbridge.
-                \n\n{0}\n\n
-                Bad match records: {1}
-            '''.format(badmatches_table, len(badmatches))
-            sendmail(
-                settings.EVERBRIDGE_TO_EMAIL, settings.EVERBRIDGE_FROM_EMAIL,
-                BODY, SUBJECT
-            )
-        else:
-            if test:
-                print('Do not send email')
-
-        phile.close()
-
-        if not test:
-            # SFTP the CSV
-            try:
-                print('sftp attempt')
-                print(filename)
-                with pysftp.Connection(**XTRNL_CONNECTION) as sftp:
-                    sftp.chdir("replace/")
-                    print("current working directory: {}".format(sftp.getcwd()))
-                    sftp.put(filename, preserve_mtime=True)
-                    print("file uploaded:")
-                    for i in sftp.listdir():
-                        print(i)
-                        print(str(sftp.lstat(i)))
-                    sftp.close()
-                print("sftp put success: {}".format(key))
-            except Exception, e:
-                print('sftp put fail [{}]: {}'.format(key, e))
-                SUBJECT = '[Everbridge SFTP] {} failed'.format(key)
-                BODY = 'Unable to PUT upload to Everbridge server.\n\n{}'.format(
-                    str(e)
-                )
+                badmatches_table = ''.join(badmatches)
+                if test:
+                    print("badmatches = \n{}".format(badmatches))
+            if badmatches:
+                if test:
+                    print("badmatches_table = \n{}".format(badmatches_table))
+                    print("length of badmatches = {}.".format(len(badmatches)))
+                SUBJECT = '[Everbridge] Bad match'
+                BODY = '''
+                    A bad match exists in the file we are sending to Everbridge.
+                    \n\n{0}\n\n
+                    Bad match records: {1}
+                '''.format(badmatches_table, len(badmatches))
                 sendmail(
-                    settings.EVERBRIDGE_TO_EMAIL,settings.EVERBRIDGE_FROM_EMAIL,
-                    SUBJECT, BODY
+                    settings.EVERBRIDGE_TO_EMAIL, settings.EVERBRIDGE_FROM_EMAIL,
+                    BODY, SUBJECT
                 )
+            else:
+                if test:
+                    print('Do not send email')
+
+            phile.close()
+
+            if not test:
+                # SFTP the CSV
+                try:
+                    print('sftp attempt')
+                    print(filename)
+                    with pysftp.Connection(**XTRNL_CONNECTION) as sftp:
+                        sftp.chdir("replace/")
+                        print("current working directory: {}".format(sftp.getcwd()))
+                        sftp.put(filename, preserve_mtime=True)
+                        print("file uploaded:")
+                        for i in sftp.listdir():
+                            print(i)
+                            print(str(sftp.lstat(i)))
+                        sftp.close()
+                    print("sftp put success: {}".format(key))
+                except Exception, e:
+                    print('sftp put fail [{}]: {}'.format(key, e))
+                    SUBJECT = '[Everbridge SFTP] {} failed'.format(key)
+                    BODY = 'Unable to PUT upload to Everbridge server.\n\n{}'.format(
+                        str(e)
+                    )
+                    sendmail(
+                        settings.EVERBRIDGE_TO_EMAIL,settings.EVERBRIDGE_FROM_EMAIL,
+                        SUBJECT, BODY
+                    )
 
     print "Done"
 
