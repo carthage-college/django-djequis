d9bfbc0d9daf7aa4cf75fabda7bbbc4a4e621cc3
plungerman <plungerman@gmail.com>
2019-02-27 13:58:25 -0600

    code clean up of papercut scripts; updated last row of papercut CSV file to include year/month and dept code

--- djequis/bin/papercut.py
+++ djequis/bin/papercut.py
@@ -2,7 +2,6 @@ import os
 import sys
 import csv
 import argparse
-from datetime import date
 from dateutil.relativedelta import relativedelta
 import time
 import shutil
@@ -25,29 +24,14 @@ django.setup()
 
 # django settings for script
 from django.conf import settings
-from django.db import connections
-
-# informix environment
-os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
-os.environ['DBSERVERNAME'] = settings.DBSERVERNAME
-os.environ['INFORMIXDIR'] = settings.INFORMIXDIR
-os.environ['ODBCINI'] = settings.ODBCINI
-os.environ['ONCONFIG'] = settings.ONCONFIG
-os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
-os.environ['LD_LIBRARY_PATH'] = settings.LD_LIBRARY_PATH
-os.environ['LD_RUN_PATH'] = settings.LD_RUN_PATH
-
-from djequis.core.utils import sendmail
-
-from djzbar.utils.informix import do_sql
-from djzbar.utils.informix import get_engine
-from djzbar.settings import INFORMIX_EARL_TEST
-from djzbar.settings import INFORMIX_EARL_PROD
 
 from djtools.fields import TODAY
 from djtools.utils.mail import send_mail
 
-DEBUG = settings.INFORMIX_DEBUG
+TO = settings.PAPERCUT_TO_EMAIL
+FROM = settings.PAPERCUT_FROM_EMAIL
+BCC = settings.PAPERCUT_BCC_EMAIL
+TEMPLATE = 'papercut/email.html'
 
 # set up command-line options
 desc = """
@@ -66,76 +50,93 @@ def main():
     datetimestr = time.strftime("%Y%m%d%H%M%S")
     # Returns the same day of last month if possible otherwise end of month
     # (eg: March 31st->29th Feb an July 31st->June 30th)
-    last_month = date.today() - relativedelta(months=1)
+    last_month = TODAY - relativedelta(months=1)
     # create string of short notation for month name and year
-    monthYear = format(last_month, '%b%Y')
+    month_year = format(last_month, '%b%Y')
     # source path (/data2/www/data/papercut/) to find the .csv file
-    source_dir = ('{0}'.format(settings.PAPERCUT_CSV_OUTPUT))
-    # current working directory where the papercut.py resides
-    current_dir = os.getcwd()
+    source_dir = ('{}'.format(settings.PAPERCUT_CSV_OUTPUT))
     # list files within the source_dir
     listfiles = os.listdir(source_dir)
+    # file flag to indicate if we found a csv file or not
+    phile = False
     for localfile in listfiles:
         # Local Path == /data2/www/data/papercut/{filename.csv}
         localpath = source_dir + localfile
         if localfile.endswith(".csv"):
-            # set archive path and new filename that it will be renamed to when archived
-            # /data2/www/data/papercut_archives/
+            print("localpath = {}".format(localpath))
+            # set archive path and new filename to which it will be renamed
+            # when archived in /data2/www/data/papercut_archives/
             archive_destination = ('{0}modified_papercut_bak_{1}.csv'.format(
                 settings.PAPERCUT_CSV_ARCHIVED, datetimestr
             ))
+            print("archive_destination = {}".format(archive_destination))
             # rename file to be processed
             # /data2/www/data/papercut/papercut.csv
-            orig_papercut_file = ('{0}papercut.csv'.format(source_dir))
+            orig_papercut_file = ('{}papercut.csv'.format(source_dir))
+            print("orig_papercut_file = {}".format(orig_papercut_file))
             # file name for new file being created
             # /data2/www/data/papercut/monthly-papercut.csv
-            modified_papercut_file = ('{0}monthly-papercut.csv'.format(source_dir))
+            modified_papercut_file = (
+                '{}monthly-papercut.csv'.format(source_dir)
+            )
+            print("modified_papercut_file = {}".format(modified_papercut_file))
             # the filename renamed to papercut.csv
             shutil.move(localpath, orig_papercut_file)
             # modified papercut output csv file
             total_cost = 0
             with open(modified_papercut_file, 'wb') as modified_papercut_csv:
                 writer = csv.writer(modified_papercut_csv)
-                # open original papercut input csv file for reading 
+                # open original papercut input csv file for reading
                 with open(orig_papercut_file,'r') as orig_papercut_csv:
                     for i in range(2):
                         orig_papercut_csv.next()
-                    #print(i)
                     reader = csv.DictReader(orig_papercut_csv, delimiter=',')
-                    # creates header row in csv file
-                    # headrow = ("Description", "PostngAccnt", "Amount") 
-                    # writes file header elements
-                    # writer.writerow(headrow) Not going to write header row per email from Ryan Ade
                     for row in reader:
                         try:
-                            # split account name to remove shared account parent name
-                            ####################################################
-                            # the objective is to remove everything before the slash (/)
-                            # including and after a certain character in the string
-                            # \s* will helps to match also the preceding vertical or horizontal space character
-                            ####################################################
-                            #accountName2 = re.sub(r'(.*)/(.*)(.*)#(.*)',row['Shared Account Parent Name'])
-                            accountName = re.sub(r'\s*#.*', '', row['Shared Account Parent Name'].split('/',1)[1])
+                            # split account name to remove shared account
+                            # parent name
+                            ###################################################
+                            # the objective is to remove everything before the
+                            # slash (/) including and after a certain character
+                            # in the string \s* will helps to match also the
+                            # preceding vertical or horizontal space character
+                            ###################################################
+                            #accountName2 = re.sub(
+                                #r'(.*)/(.*)(.*)#(.*)',
+                                #row['Shared Account Parent Name']
+                            #)
+                            accountName = re.sub(
+                              r'\s*#.*', '',
+                              row['Shared Account Parent Name'].split('/',1)[1]
+                            )
                             #print(accountName)
                             #print row['Cost']
                             # sum of the Cost field
                             total_cost += float(row['Cost'])
-                            csv_line = ("{0} print-copy".format(monthYear),
-                                         accountName, row['Cost']
-                                         )
+                            csv_line = (
+                                "{} print-copy".format(month_year), accountName,
+                                row['Cost']
+                            )
                             writer.writerow(csv_line)
                         except Exception as e:
-                            #print "Exception: {0}".format(str(e))
-                            # Email there was an exception error while processing .csv
-                            SUBJECT = '[Papercut] modified file'
-                            BODY = "There was an exception error: {0}".format(str(e))
-                            sendmail(settings.PAPERCUT_TO_EMAIL,settings.PAPERCUT_FROM_EMAIL,
-                                     BODY, SUBJECT
-                                     )
+                            #print "Exception: {}".format(str(e))
+                            # Email there was an exception error while
+                            # processing .csv
+                            SUBJECT = "[Papercut] modified file"
+                            BODY = "There was an exception error: {}".format(
+                                str(e)
+                            )
+                            send_mail(
+                                None,TO,SUBJECT,FROM,TEMPLATE,BODY,bcc=BCC
+                            )
                     # convert positve interger to negative interger
                     total_cost = (total_cost * -1)
                     # writes the last line for the total cost
-                    writer.writerow(["", "", (total_cost)])
+                    csv_line = (
+                        "{} print-copy".format(month_year), "1-509-73140",
+                        (total_cost)
+                    )
+                    writer.writerow(csv_line)
                 # close orig_papercut_csv
                 orig_papercut_csv.close()
             # close modified_papercut_csv
@@ -146,26 +147,21 @@ def main():
             shutil.copy(modified_papercut_file, archive_destination)
             # send email with file attachment
             file_attach = modified_papercut_file
-            request = None
-            recipients = settings.PAPERCUT_TO_EMAIL
             subject = "[Papercut] with attachment"
-            femail = settings.PAPERCUT_FROM_EMAIL
-            template = 'papercut/email.html'
-            bcc = settings.PAPERCUT_BCC_EMAIL
-            send_mail(request, recipients, subject, femail, template, bcc, attach=file_attach)
+            send_mail(
+                None, TO, subject, FROM, TEMPLATE, bcc=BCC,
+                attach=file_attach
+            )
             # delete monthly-papercut.csv file
             os.remove(modified_papercut_file)
+            phile = False
             # break tells whether we have found the .csv file
             break
-    # The else attached to a for will be run whenever the break inside the loop is not executed
-    # the else is run because we didn't find the .csv we were searching for
-    else:
+    if not phile:
         # no file was found send email
-        SUBJECT = '[Papercut] failed no file found'
+        SUBJECT = '[Papercut] failed: no file found'
         BODY = 'No .csv file was found'
-        sendmail(settings.PAPERCUT_TO_EMAIL,settings.PAPERCUT_FROM_EMAIL,
-            BODY, SUBJECT
-        )
+        send_mail(None, TO, SUBJECT, FROM, TEMPLATE, BODY, bcc=BCC)
 
 if __name__ == "__main__":
     args = parser.parse_args()
--- djequis/sql/papercut_glrec.py
+++ djequis/sql/papercut_glrec.py
@@ -4,8 +4,12 @@ GET_GL_ACCTS = '''
     FROM
         gla_rec
     WHERE
-        fscl_yr = CASE WHEN TODAY < TO_DATE(YEAR(TODAY) || '-07-01', '%Y-%m-%d')    THEN MOD(YEAR(TODAY) - 1, 100) || MOD(YEAR(TODAY), 100)
-                                                                                    ELSE MOD(YEAR(TODAY), 100) || MOD(YEAR(TODAY) + 1, 100)
+        fscl_yr = CASE
+                    WHEN TODAY < TO_DATE(YEAR(TODAY) || '-07-01', '%Y-%m-%d')
+                  THEN
+                    MOD(YEAR(TODAY) - 1, 100) || MOD(YEAR(TODAY), 100)
+                  ELSE
+                    MOD(YEAR(TODAY), 100) || MOD(YEAR(TODAY) + 1, 100)
                   END
     AND ((obj= 73140 AND fund IN (1,2))
     OR (func = 856 AND fund <> 1))
