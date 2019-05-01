2e80975cbad5a936e4851346c0616d05c4f37ecc
dsullivan <dsullivan@carthage.edu>
2019-05-01 13:33:18 -0500

    SQL updated to use provisioning_vw as the primary source of the data.

--- djequis/handshake/handshake_sql.py
+++ djequis/handshake/handshake_sql.py
@@ -1,8 +1,7 @@
 from sqlalchemy import text
 
 HANDSHAKE_QUERY = '''
-SELECT Distinct 
-	TRIM(NVL(EML.line1,'')) AS email_address, 
+SELECT Distinct 	TRIM(NVL(EML.line1,'')) AS email_address, 
 	TO_CHAR(PER.id) AS username, 
 	TRIM(CV.ldap_name) AS auth_identifier, 
 	'' AS card_id,  --Prox ID?  but not sure
@@ -27,15 +26,16 @@ SELECT Distinct
 	   		OR PER.to_alum_date IS NOT NULL) THEN 'Alumni'  
 		ELSE '' 
 		END AS school_year_name, 
-  	--Associates, Certificate, Advanced Certificate, Bachelors, Masters, 
+   	--Associates, Certificate, Advanced Certificate, Bachelors, Masters, 
   		--Doctorate, Postdoctoral Studies, Non-Degree Seeking.
-	CASE 
+	 CASE 
 		WHEN (PER.deg in ('BS','BA')) THEN 'Bachelors' 
 	   	WHEN (PER.deg = 'CERT') THEN 'Certificate' 
 		WHEN (PER.deg in ('MBDI', 'MBA', 'MSW', 'MS', 'MM', 'MED')) THEN 'Masters' 
 	   	WHEN (PER.deg = 'NONE') OR (CL.CL = 'ND') THEN 'Non-Degree Seeking' 
 	ELSE ''   --	ELSE DEG.txt  
-		END AS education_level_name,
+		END AS education_level_name, 
+	
 	SAR.cum_gpa AS cumulative_gpa,
 	'' AS department_gpa,
 	TRIM(MAJ1.txt) as primary_major_name,
@@ -60,41 +60,33 @@ SELECT Distinct
 	--send the **CLEAR** for a period of time.
 	--This should insure that old leave dates are erased and won't 
 	--send **CLEAR** for anyone else
- 	CASE 
+	CASE 
 		WHEN PER.prog = 'GRAD' and PER.acst = 'GOOD' and PER.lv_date is null 
-			and PER.acad_date > TODAY-5 THEN '**CLEAR**'  
+			and PER.acad_date > TODAY-3 THEN '**CLEAR**'  
 	 	WHEN PER.prog != 'GRAD' and PER.acst = 'READ' and PER.lv_date is null 
-	 		and PER.acad_date > TODAY-5 THEN '**CLEAR**'  
-		ELSE TO_CHAR(PER.lv_date) 
-		END AS end_date,
---	PER.lv_date AS end_date,
+	 		and PER.acad_date > TODAY-3 THEN '**CLEAR**'  
+		ELSE TO_CHAR(PER.lv_date) END 
+		AS end_date,
 	'' as currently_attending,
 	'' AS campus_name,
 	'' AS opt_cpt_eligible,  --(Optional Practical Training) and CPT (Curricular 
 		--Practical Training). Assigning TRUE
 	'' AS ethnicity,
 	'' as gender,
-		'FALSE' as disabled,
-	CASE WHEN AID.id IS NULL THEN 'FALSE' ELSE 'TRUE' 
+	'FALSE' as disabled,
+ 	CASE WHEN AID.id IS NULL THEN 'FALSE' ELSE 'TRUE' 
 		END AS work_study_eligible, 
-		''||CASE WHEN LENGTH(NVL(CT1.txt,'')) = 0 then '' ELSE TRIM(CT1.txt) END
- 		||CASE  
-		    WHEN CT2.txt IS NOT NULL 
-		    AND LENGTH(NVL(CT1.txt,'')) = 0 THEN TRIM(CT2.txt) 
-		    WHEN CT2.txt IS NOT NULL 
-		    AND LENGTH(NVL(CT1.txt,'')) > 0 THEN ';'||TRIM(CT2.txt) 
-			ELSE '' END
- 		||CASE  
-		    WHEN CT3.txt IS NOT NULL 
-		    AND LENGTH(NVL(CT1.txt,'')||NVL(CT1.txt,'')) = 0 THEN TRIM(CT2.txt) 
-		    WHEN CT3.txt IS NOT NULL 
-		    AND LENGTH(NVL(CT1.txt,'')||NVL(CT1.txt,'')) > 0 THEN ';'||TRIM(CT2.txt) 
-			ELSE '' END
+		''||TRIM(NVL(CT1.txt,''))
+		||CASE WHEN CT2.txt IS NULL OR TRIM(CT2.txt) = '' THEN '' 
+			ELSE ';'||TRIM(CT2.txt) END 
+		||CASE WHEN CT3.txt IS NULL OR TRIM(CT3.txt) = '' THEN '' 
+			ELSE ';'||TRIM(CT3.txt) END 
 		||CASE WHEN (NVL(CT1.txt,'') != '' AND NVL(SPORT.descr,'') != '') 
 			THEN ';' ELSE '' END
 		||CASE WHEN SPORT.descr IS NULL OR TRIM(SPORT.descr) = '' THEN '' 
 			ELSE TRIM(SPORT.descr) END 
 	as system_label_names, 
+
 	 CASE WHEN len(REPLACE(TRIM(NVL(CELL.phone,'')), '-', '')) = 10 
 		THEN REPLACE(TRIM(NVL(CELL.phone,'')), '-', '') 
 		ELSE 
@@ -109,133 +101,169 @@ SELECT Distinct
 		END AS athlete, 
 	CASE TRIM(PRO.vet) WHEN 'N' THEN 'FALSE' WHEN 'Y' THEN 'TRUE' ELSE '' 
 		END AS veteran, 
-	TRIM(IR.city)||', '||TRIM(ST.txt)||', '||ir.ctry 
+	TRIM(IR.city)||', '||TRIM(ST.txt)||', '||IR.ctry 
 		AS hometown_location_attributes, 
 	'FALSE' AS eu_gdpr_subject   
 FROM
-		 --Students
-	  		(select id, prog, acst, subprog, deg,  site, cl, adm_yr, 
-			adm_date, enr_date, acad_date, major1, major2, major3, adv_id,
+	(
+	SELECT id, prog, acst, subprog, deg, site, cl, adm_yr, adm_date, enr_date, 
+		acad_date, major1, major2, major3, adv_id, conc1, conc2, conc3, 
+		minor1, minor2,
+		minor3, deg_grant_date, vet_ben, lv_date, to_alum, to_alum_date, cohort_yr,
+		honors, tle, nurs_prog, nurs_prog_date, unmet_need
+		FROM (
+		SELECT unique PV.id, PV.student, PV.faculty, PV.staff, PR.prog, PR.acst, 
+			PR.subprog, PR.deg, PR.site, PR.cl, PR.adm_yr, PR.adm_date, PR.enr_date,
+			PR.acad_date, PR.major1, PR.major2, PR.major3, adv_id, conc1, conc2, conc3,
+			minor1, minor2, minor3, deg_grant_date, vet_ben, lv_date, to_alum,
+			to_alum_date, cohort_yr, honors, tle, nurs_prog, nurs_prog_date, unmet_need,
+			row_number() over ( partition BY PR.id
+			ORDER BY 
+				CASE when PR.prog = 'GRAD' then 1 
+					when PR.prog = 'UNDG' then 2 
+					when PR.prog = 'PRDV' then 3 
+					WHEN PR.PROG = 'ACT' THEN 4 
+					when PR.prog = 'PARA' then 5 
+					else 9 end ) 
+					as row_num
+		FROM provisioning_vw PV
+		LEFT JOIN prog_enr_rec PR
+		        ON PV.id = PR.id
+		WHERE PV.student IN ('prog', 'stu', 'reg_clear')
+			AND PR.acst IN ('GOOD' ,'LOC' ,'PROB' ,'PROC' , 'PROR' ,'READ' ,
+			'RP','SAB','SHAC' ,'SHOC', 'GRAD')
+		    AND (PR.subprog NOT IN ('KUSD', 'UWPK', 'YOP', 'ENRM'))
+		    AND (PR.CL != 'UP')
+		    AND (PR.lv_date IS NULL)
+		    AND (PR.deg_grant_date IS NULL)
+		    --SCREEN OUT FIRST TIME FROSH UNTIL AUG 1
+			AND PV.ID NOT IN 
+				(Select DISTINCT id from prog_enr_rec 
+				WHERE CL in ('FF','FN') AND acst = 'GOOD'
+				AND MONTH(TODAY) < 8 
+				AND MONTH(TODAY) > 5) --Keeps incoming frosh out during summer
+			--and PR.id = 921099
+
+		UNION
+
+		SELECT unique PR.id, '' student, '' faculty, '' staff, PR.prog, PR.acst, 
+			PR.subprog, PR.deg, PR.site, PR.cl, PR.adm_yr, PR.adm_date,
+			PR.enr_date, PR.acad_date, PR.major1, PR.major2, PR.major3, adv_id, 
 			conc1, conc2, conc3, minor1, minor2, minor3, deg_grant_date, 
 			vet_ben, lv_date, to_alum, to_alum_date, cohort_yr, honors, tle, 
-			nurs_prog, nurs_prog_date, unmet_need  from 
-				(select unique id, prog, acst, subprog, deg,  site, cl, adm_yr, 
-			adm_date, enr_date, acad_date, major1, major2, major3, adv_id,
-			conc1, conc2, conc3, minor1, minor2, minor3, deg_grant_date, 
-			vet_ben, lv_date, to_alum, to_alum_date, cohort_yr, honors, tle, 
-			nurs_prog, nurs_prog_date, unmet_need,   
-				row_number() over
-					(
-						partition by id
-						order by CASE when prog = 'GRAD' then 1 
-							when prog = 'UNDG' then 2 
-							when prog = 'PRDV' then 3 
-							WHEN PROG = 'ACT' THEN 4 
-							when prog = 'PARA' then 5 
-							else 9 end
-					) as row_num	
-	        	from prog_enr_rec
-	        	where prog_enr_rec.acst IN ('GOOD' ,'LOC' ,'PROB' ,'PROC' ,
-	        	'PROR' ,'READ' ,'RP' ,'SAB' ,'SHAC' ,'SHOC', 'GRAD')
-   				AND (subprog not IN ('KUSD', 'UWPK', 'YOP', 'ENRM'))
-				AND (CL != 'UP')
-	        	AND (prog_enr_rec.lv_date IS NULL 
-	        	OR prog_enr_rec.lv_date > TODAY-3)
-	     		) rnk_prog
-			where row_num = 1		
- 			) PER 
-	 			INNER JOIN	id_rec		IR	ON	PER.id			=	IR.id
-				LEFT JOIN (SELECT id, aa, line1 
-						FROM aa_rec 
-						WHERE aa = 'EML1' AND	TODAY BETWEEN beg_date 
-						AND NVL(end_date, TODAY) 
-					) EML ON EML.id = PER.id
-				LEFT JOIN		(
-					SELECT id.id,
-					    replace(replace(replace(replace(replace(replace(replace(
-						multiset(      
-						SELECT DISTINCT trim(a) from 
-							(SELECT invl_table.txt a 
-								 FROM involve_rec 
-					 			 	JOIN invl_table 
-									ON invl_table.invl=involve_rec.invl 
-									WHERE id=id.id 
-									AND invl_table.sanc_sport = 'Y' 
-									ORDER BY invl_table.txt)
-							)::lvarchar,'MULTISET{'), 'ROW'), '}'),"')",''), 
-							"('",''), ',',';'), "''","'")
-							DESCR
-							FROM id_rec id	
-						) SPORT ON SPORT.id = PER.id						    
-				INNER JOIN	cvid_rec CV	ON	PER.id = CV.cx_id
-				LEFT JOIN st_table ST ON ST.st = IR.st 
-				INNER JOIN	cl_table CL	ON	PER.cl = CL.cl
-				LEFT JOIN major_table MAJ1	ON	PER.major1 = MAJ1.major
-				LEFT JOIN major_table MAJ2	ON	PER.major2 = MAJ2.major
-				LEFT JOIN major_table MAJ3	ON	PER.major3 = MAJ3.major
-				LEFT JOIN deg_table	DEG	ON	PER.deg = DEG.deg
-				INNER JOIN adm_rec ADM	ON	PER.id = ADM.id
-					AND ADM.prog = PER.prog
-					AND	ADM.primary_app	=	'Y'
-				INNER JOIN	profile_rec	PRO	ON	PER.id = PRO.id
-				LEFT JOIN minor_table	MIN1  ON PER.minor1 = MIN1.minor
-				LEFT JOIN minor_table	MIN2  ON PER.minor2 = MIN2.minor
-				LEFT JOIN minor_table	MIN3  ON PER.minor3 = MIN3.minor
-			 	LEFT JOIN 
-			 		(SELECT id, aa, line1 
-						FROM aa_rec 
-						WHERE aa = 'EML1' AND	TODAY BETWEEN beg_date 
-						AND NVL(end_date, TODAY) 
-					) ADV on ADV.id = PER.adv_id
-				LEFT JOIN 
-					(SELECT a.id ID, a.aa aa, a.line1 line1, 
-					    a.phone phone, a.beg_date beg_date
-					FROM aa_rec a
-					INNER JOIN 
-						(
-					    SELECT id, MAX(beg_date) beg_date
-						    FROM aa_rec 
-						    WHERE aa = 'CELL'
-						    GROUP BY id
-							) b 
-						ON a.id = b.id AND a.beg_date = b.beg_date
-						and a.aa = 'CELL'
-					) CELL
-					ON CELL.ID = PER.ID	
-				LEFT JOIN		(
-					SELECT id
-							FROM aid_rec
-							WHERE aid	=	'FWSY'
-							AND stat	in	('A','I')
-							AND TRIM(sess) || yr	IN	
-							    (SELECT TRIM(sess) || yr 
-							    FROM cursessyr_vw)
-							GROUP BY 		id
-						)	AID	ON PER.id = AID.id
-				LEFT JOIN 
-					(SELECT id, gpa, mflag
-    					FROM degaudgpa_rec
-						WHERE mflag = 'MAJOR1' AND gpa > 0
-						) DGR
-					ON 	DGR.id = PER.ID 
-			 	LEFT JOIN 
-					(SELECT distinct sr.prog, sr.id, sr.subprog, 
-							sr.cum_gpa, sr.yr, sr.subprog
-						FROM stu_acad_rec sr, cursessyr_vw cv
-		 				WHERE sr.sess = cv.sess
-		 				AND sr.prog = cv.prog
-		 				AND sr.yr = cv.yr) SAR
-						ON SAR.id = PER.id	
-							AND SAR.prog = PER.prog	  
-					LEFT JOIN (select conc, txt from conc_table 
-						WHERE LEFT(cip_no,2) in ('51')) CT1 
-  					ON CT1.conc = PER.conc1
-					LEFT JOIN (select conc, txt from conc_table 
-						WHERE LEFT(cip_no,2) in ('51')) CT2 
-  					ON CT2.conc = PER.conc2
-					LEFT JOIN (select conc, txt from conc_table 
-						WHERE LEFT(cip_no,2) in ('51')) CT3 
-  					ON CT3.conc = PER.conc3 WHERE 
+			nurs_prog, nurs_prog_date, unmet_need, 1 row_num
+		FROM prog_enr_rec PR
+		WHERE PR.lv_date > TODAY-3
+		AND PR.subprog NOT IN ('KUSD', 'UWPK', 'YOP', 'ENRM', 'PARA')
+		
+		    ) rnk_prog
+	WHERE row_num = 1
+		
+	) PER
+
+		INNER JOIN	id_rec		IR	ON	PER.id			=	IR.id
+		
+ 		LEFT JOIN (SELECT id, aa, line1 
+				FROM aa_rec 
+				WHERE aa = 'EML1' AND	TODAY BETWEEN beg_date 
+				AND NVL(end_date, TODAY) 
+			) EML ON EML.id = PER.id 
+
+		LEFT JOIN		(
+			SELECT id.id,
+			    replace(replace(replace(replace(replace(replace(replace(
+				multiset(      
+				SELECT DISTINCT trim(a) from 
+					(SELECT invl_table.txt a 
+						 FROM involve_rec 
+			 			 	JOIN invl_table 
+							ON invl_table.invl=involve_rec.invl 
+							WHERE id=id.id 
+							AND invl_table.sanc_sport = 'Y' 
+							ORDER BY invl_table.txt)
+					)::lvarchar,'MULTISET{'), 'ROW'), '}'),"')",''), 
+					"('",''), ',',';'), "''","'")
+					DESCR
+					FROM id_rec id	
+				) SPORT ON SPORT.id = PER.id		 
+								    					
+		INNER JOIN	cvid_rec CV	ON	PER.id = CV.cx_id
+		LEFT JOIN st_table ST ON ST.st = IR.st 
+		INNER JOIN	cl_table CL	ON	PER.cl = CL.cl
+	 	LEFT JOIN major_table MAJ1	ON	PER.major1 = MAJ1.major
+		LEFT JOIN major_table MAJ2	ON	PER.major2 = MAJ2.major
+		LEFT JOIN major_table MAJ3	ON	PER.major3 = MAJ3.major
+		LEFT JOIN deg_table	DEG	ON	PER.deg = DEG.deg
+		INNER JOIN adm_rec ADM	ON	PER.id = ADM.id
+			AND ADM.prog = PER.prog
+			AND	ADM.primary_app	=	'Y'
+		INNER JOIN	profile_rec	PRO	ON	PER.id = PRO.id
+		LEFT JOIN minor_table	MIN1  ON PER.minor1 = MIN1.minor
+		LEFT JOIN minor_table	MIN2  ON PER.minor2 = MIN2.minor
+		LEFT JOIN minor_table	MIN3  ON PER.minor3 = MIN3.minor 
+
+	  	LEFT JOIN 
+	 		(SELECT id, aa, line1 
+				FROM aa_rec 
+				WHERE aa = 'EML1' AND	TODAY BETWEEN beg_date 
+				AND NVL(end_date, TODAY) 
+			) ADV on ADV.id = PER.adv_id		 			
+		
+		
+	 	LEFT JOIN 
+			(SELECT a.id ID, a.aa aa, a.line1 line1, 
+			    a.phone phone, a.beg_date beg_date
+			FROM aa_rec a
+			INNER JOIN 
+				(
+			    SELECT id, MAX(beg_date) beg_date
+				    FROM aa_rec 
+				    WHERE aa = 'CELL'
+				    GROUP BY id
+					) b 
+				ON a.id = b.id AND a.beg_date = b.beg_date
+				and a.aa = 'CELL'
+			) CELL
+			ON CELL.ID = PER.ID	 
+
+		LEFT JOIN		(
+			SELECT id
+					FROM aid_rec
+					WHERE aid	=	'FWSY'
+					AND stat	in	('A','I')
+					AND TRIM(sess) || yr	IN	
+					    (SELECT TRIM(sess) || yr 
+					    FROM cursessyr_vw)
+					GROUP BY 		id
+				)	AID	ON PER.id = AID.id	 	
+							
+		LEFT JOIN 
+			(SELECT id, gpa, mflag
+	    		FROM degaudgpa_rec
+    			WHERE mflag = 'MAJOR1' AND gpa > 0
+    			) DGR
+				ON 	DGR.id = PER.ID  
+
+		LEFT JOIN 
+			(SELECT distinct sr.prog, sr.id, sr.subprog, 
+				sr.cum_gpa, sr.yr, sr.subprog
+			FROM stu_acad_rec sr, cursessyr_vw cv
+			WHERE sr.sess = cv.sess
+			AND sr.prog = cv.prog
+			AND sr.yr = cv.yr) SAR
+  			ON SAR.id = PER.id	
+  			AND SAR.prog = PER.prog	   
+		LEFT JOIN (select conc, txt from conc_table 
+			WHERE LEFT(cip_no,2) in ('51')) CT1 
+			ON CT1.conc = PER.conc1
+		LEFT JOIN (select conc, txt from conc_table 
+			WHERE LEFT(cip_no,2) in ('51')) CT2 
+			ON CT2.conc = PER.conc2
+		LEFT JOIN (select conc, txt from conc_table 
+			WHERE LEFT(cip_no,2) in ('51')) CT3 
+			ON CT3.conc = PER.conc3 
+			
+WHERE 
 	EML.line1 IS NOT NULL
 --LIMIT 10
 '''
