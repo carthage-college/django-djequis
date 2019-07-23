974aa48d0bb031d6f6b17e62e282a0d1562235bb
dsullivan <dsullivan@carthage.edu>
2019-07-23 13:11:28 -0500

    formatting and tightened up csv write code

--- djequis/adirondack/adirondack_applications_api.py
+++ djequis/adirondack/adirondack_applications_api.py
@@ -6,19 +6,17 @@ import json
 import os
 import requests
 import csv
-
-# django settings for shell environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
-
 # prime django
 import django
-django.setup()
-
 # django settings for script
 from django.conf import settings
 from djequis.core.utils import sendmail
 from adirondack_utilities import fn_write_error, fn_write_application_header, \
     fn_get_utcts
+
+# django settings for shell environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
+django.setup()
 
 # set up command-line options
 desc = """
@@ -42,81 +40,68 @@ def encode_rows_to_utf8(rows):
 
 
 def main():
-
     try:
 
-      utcts = fn_get_utcts()
-      hashstring = str(utcts) + settings.ADIRONDACK_API_SECRET
-      # print("Hashstring = " + hashstring)
+        utcts = fn_get_utcts()
+        hashstring = str(utcts) + settings.ADIRONDACK_API_SECRET
+        # print("Hashstring = " + hashstring)
 
-      # Assumes the default UTF-8
-      hash_object = hashlib.md5(hashstring.encode())
-      print(hash_object.hexdigest())
+        # Assumes the default UTF-8
+        hash_object = hashlib.md5(hashstring.encode())
+        print(hash_object.hexdigest())
 
-      # sendtime = datetime.now()
-      # print("Time of send = " + time.strftime("%Y%m%d%H%M%S"))
+        # sendtime = datetime.now()
+        # print("Time of send = " + time.strftime("%Y%m%d%H%M%S"))
 
-      url = "https://carthage.datacenter.adirondacksolutions.com/" \
+        url = "https://carthage.datacenter.adirondacksolutions.com/" \
             "carthage_thd_test_support/apis/thd_api.cfc?" \
             "method=housingAPPLICATIONS&" \
-            "Key="+settings.ADIRONDACK_API_SECRET+"&" \
-            "utcts="+str(utcts)+"&" \
-            "h="+hash_object.hexdigest()+"&" \
+            "Key=" + settings.ADIRONDACK_API_SECRET + "&" \
+            "utcts=" + str(utcts) + "&" \
+            "h=" + hash_object.hexdigest() + "&" \
             "TimeFrameNumericCode=" + "RA 2019"
-            # + "&" \
-            # "studentNumber=-1"
-            # + "&"
-            # "ApplicationTypeName= "
-              # + "&"
-              # "APP_COMPLETE= "
-              # + "&"
-              # "APP_CANCELED= "
-              # + "&"
-              # "DEPOSIT= "
-              # + "&"
-              # "UNDERAGE= "
+        # + "&" \
+        # "studentNumber=-1"
+        # + "&"
+        # "ApplicationTypeName= "
+        # + "&"
+        # "APP_COMPLETE= "
+        # + "&"
+        # "APP_CANCELED= "
+        # + "&"
+        # "DEPOSIT= "
+        # + "&"
+        # "UNDERAGE= "
 
-      # print("URL = " + url)
+        # print("URL = " + url)
 
-      response = requests.get(url)
-      x = json.loads(response.content)
-      # print(x)
-      y = (len(x['DATA'][0][0]))
-      if not x['DATA']:
-          print("No match")
-      else:
-          fn_write_application_header()
-          print("Start Loop")
-          with open(settings.ADIRONDACK_APPLICATONS, 'ab') as output:
-              for i in x['DATA']:
-                  rec = []
-                  rec.append(i[0])
-                  rec.append(i[1])
-                  rec.append(i[2])
-                  rec.append(i[3])
-                  rec.append(i[4])
-                  rec.append(i[5])
-                  rec.append(i[6])
-                  rec.append(i[7])
-                  rec.append(i[8])
-                  rec.append(i[9])
-                  rec.append(i[10])
-                  rec.append(i[11])
-                  rec.append(i[12])
-                  rec.append(i[13])
-                  rec.append(i[14])
-                   # print("Rec = " + str(rec))
-                  csvWriter = csv.writer(output,
-                                         quoting=csv.QUOTE_NONNUMERIC)
-                  csvWriter.writerow(rec)
+        response = requests.get(url)
+        x = json.loads(response.content)
+        # print(x)
+        # y = (len(x['DATA'][0][0]))
+        if not x['DATA']:
+            print("No match")
+        else:
+            fn_write_application_header()
 
-          # Validate if the stu_serv_rec exists first                            ])
-          # update stu_serv_rec id, sess, yr, rxv_stat, intend_hsg, campus, bldg, room, bill_code
+            print("Start Loop")
+            with open(settings.ADIRONDACK_APPLICATONS, 'ab') as output:
+                for i in x['DATA']:
+                    print(i)
+                    csvWriter = csv.writer(output,
+                                           quoting=csv.QUOTE_NONNUMERIC)
+                    csvWriter.writerow(i)
+
+            # Validate if the stu_serv_rec exists first
+            # ])
+            # update stu_serv_rec id, sess, yr, rxv_stat, intend_hsg,
+            # campus, bldg, room, bill_code
 
     except Exception as e:
-          print("Error in adirondack_applicationss_api.py- Main:  " + e.message)
-          # fn_write_error("Error in adirondack_std_billing_api.py - Main: "
-          #                + e.message)
+        print("Error in adirondack_applicationss_api.py- Main:  " + e.message)
+        # fn_write_error("Error in adirondack_std_billing_api.py - Main: "
+        #                + e.message)
+
 
 if __name__ == "__main__":
     main()
