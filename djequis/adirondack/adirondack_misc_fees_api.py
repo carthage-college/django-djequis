7b0f5823142ba53708ab8b552214eaa994fcae6a
dsullivan <dsullivan@carthage.edu>
2019-07-23 13:12:16 -0500

    formatting

--- djequis/adirondack/adirondack_misc_fees_api.py
+++ djequis/adirondack/adirondack_misc_fees_api.py
@@ -8,21 +8,21 @@ import json
 import requests
 import csv
 import logging
+import django
 from logging.handlers import SMTPHandler
+from django.conf import settings
+from djequis.core.utils import sendmail
+from adirondack_utilities import fn_write_error, fn_write_misc_header, \
+    fn_sendmailfees, fn_get_utcts
 
 # django settings for shell environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djequis.settings")
 
 # prime django
-import django
 
 django.setup()
 
 # django settings for script
-from django.conf import settings
-from djequis.core.utils import sendmail
-from adirondack_utilities import fn_write_error, fn_write_misc_header, \
-    fn_sendmailfees, fn_get_utcts
 
 # set up command-line options
 desc = """
@@ -30,6 +30,7 @@ desc = """
 """
 # Test with this then remove, use the standard logging mechanism
 logger = logging.getLogger(__name__)
+
 
 def main():
     try:
@@ -48,17 +49,15 @@ def main():
               "carthage_thd_test_support/apis/thd_api.cfc?" \
               "method=studentBILLING&" \
               "Key=" + settings.ADIRONDACK_API_SECRET + "&" \
-                                                        "utcts=" + str(
-            utcts) + "&" \
-                     "h=" + hash_object.hexdigest() + "&" \
-                                                      "AccountCode=2010," \
-                                                      "2011,2031,2040"
+              "utcts=" + str(utcts) + "&" \
+              "h=" + hash_object.hexdigest() + "&" \
+              "AccountCode=2010," \
+              "2011,2031,2040"
         # print("URL = " + url)
 
         response = requests.get(url)
         x = json.loads(response.content)
         # print(x)
-        y = (len(x['DATA'][0][0]))
         if not x['DATA']:
             print("No data")
         else:
@@ -91,9 +90,9 @@ def main():
             SUBJECT = 'Housing Miscellaneous Fees'
             BODY = 'There are housing fees to process via ASCII post'
             fn_sendmailfees(settings.ADIRONDACK_TO_EMAIL,
-                     settings.ADIRONDACK_FROM_EMAIL,
-                     BODY, SUBJECT
-                     )
+                            settings.ADIRONDACK_FROM_EMAIL,
+                            BODY, SUBJECT
+                            )
 
     except Exception as e:
         print("Error in adirondack_misc_fees_api.py- Main:  " + e.message)
@@ -102,5 +101,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     sys.exit(main())
