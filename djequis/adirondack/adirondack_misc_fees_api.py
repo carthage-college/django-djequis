f7eee4adbfb620379c38b6803c5dcbda2e2a95ef
dsullivan <dsullivan@carthage.edu>
2019-08-01 15:30:40 -0500

    added api params related to already exported records as well as some comments

--- djequis/adirondack/adirondack_misc_fees_api.py
+++ djequis/adirondack/adirondack_misc_fees_api.py
@@ -62,43 +62,47 @@ def main():
         # Would need to delete all, write whatever comes back, test for
         # existence of each and mail each
         url = "https://carthage.datacenter.adirondacksolutions.com/" \
-              "carthage_thd_test_support/apis/thd_api.cfc?" \
-              "method=studentBILLING&" \
-              "Key=" + settings.ADIRONDACK_API_SECRET + "&" \
-              "utcts=" + str(utcts) + "&" \
-              "h=" + hash_object.hexdigest() + "&" \
-              "AccountCode=2010,2040,2011,2031"
-              # + "&" \
-              # "ExportCharges=-1"
+            "carthage_thd_test_support/apis/thd_api.cfc?" \
+            "method=studentBILLING&" \
+            "Key=" + settings.ADIRONDACK_API_SECRET + "&" \
+            "utcts=" + str(utcts) + "&" \
+            "h=" + hash_object.hexdigest() + "&" \
+            "AccountCode=2010,2040,2011,2031" \
+            + "&" + "Exported=0" \
+            + "&" + "ExportCharges=-1" \
+            + "&" + "STUDENTNUMBER=ASI000987654321"
 
-        # print("URL = " + url)
+            # DEFINIIONS
+            # Exported: -1 exported will be included, 0 only non-exported
+            # ExportCharges: if -1 then charges will be marked as exported
+
+            # print("URL = " + url)
 
         response = requests.get(url)
         x = json.loads(response.content)
         # print(x)
         if not x['DATA']:
-            print("No data")
+            print("No new data found")
         else:
-
             files = os.listdir(settings.ADIRONDACK_TXT_OUTPUT)
 
             for f in files:
                 if f.find('misc_housing') != -1:
-                    ext=f.find(".csv")
+                    ext = f.find(".csv")
 
                     # print("source = " + settings.ADIRONDACK_TXT_OUTPUT+f)
-                    # print("destintation = " + settings.ADIRONDACK_ARCHIVED+f[:ext] + "_" + timestr + f[ext:])
-                    shutil.move(settings.ADIRONDACK_TXT_OUTPUT+f,
-                                settings.ADIRONDACK_ARCHIVED+f[:ext] + "_" +
+                    # print("destintation = " + settings.ADIRONDACK_ARCHIVED+
+                    # f[:ext]
+                    # + "_" + timestr + f[ext:])
+                    shutil.move(settings.ADIRONDACK_TXT_OUTPUT + f,
+                                settings.ADIRONDACK_ARCHIVED + f[:ext] + "_" +
                                 timestr + f[ext:])
-
 
             for i in x['DATA']:
                 # Marietta needs date, description,account number, amount,
                 # ID, totcode, billcode, term
                 rec = []
                 rec.append(i[1])
-
                 descr = str(i[5])
                 descr = descr.translate(None, '!@#$%.,')
                 rec.append(descr.strip())
@@ -111,32 +115,29 @@ def main():
                 rec.append(totcode)
                 rec.append(i[4][:2] + i[4][5:])
 
-
                 fee_file = settings.ADIRONDACK_TXT_OUTPUT + totcode + "_" + \
                            settings.ADIRONDACK_ROOM_FEES + datetimestr + ".csv"
 
                 # print(fee_file)
                 with codecs.open(fee_file, 'ab',
                                  encoding='utf-8-sig') as fee_output:
-
                     csvWriter = csv.writer(fee_output,
-                                       quoting=csv.QUOTE_NONE)
+                                           quoting=csv.QUOTE_NONE)
                     csvWriter.writerow(rec)
                 fee_output.close()
 
-            print("File created, send")
-            SUBJECT = 'Housing Miscellaneous Fees'
-            BODY = 'There are housing fees to process via ASCII post'
-            fn_sendmailfees(settings.ADIRONDACK_TO_EMAIL,
-                            settings.ADIRONDACK_FROM_EMAIL,
-                            BODY, SUBJECT
-                            )
+                print("File created, send")
+                SUBJECT = 'Housing Miscellaneous Fees'
+                BODY = 'There are housing fees to process via ASCII post'
+                fn_sendmailfees(settings.ADIRONDACK_TO_EMAIL,
+                                settings.ADIRONDACK_FROM_EMAIL,
+                                BODY, SUBJECT
+                                )
 
     except Exception as e:
         print("Error in adirondack_misc_fees_api.py- Main:  " + e.message)
         fn_write_error("Error in adirondack_std_billing_api.py - Main: "
-                       + e.message)
-
+                   + e.message)
 
 if __name__ == "__main__":
     sys.exit(main())
