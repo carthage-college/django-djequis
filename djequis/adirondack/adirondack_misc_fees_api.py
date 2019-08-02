279bc481c52df8b438af3174095620fc2d6bbed0
dsullivan <dsullivan@carthage.edu>
2019-08-02 15:51:25 -0500

    Added concepts to further verify that a record has been both pulled from adirondack and added to a csv file

--- djequis/adirondack/adirondack_misc_fees_api.py
+++ djequis/adirondack/adirondack_misc_fees_api.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 import time
 import datetime
+from datetime import datetime, timedelta
 import codecs
 import hashlib
 import json
@@ -35,6 +36,16 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    #Working Assumptions:
+    # We will do a mass pull around April 20 when the returning students
+    # have been assigned.
+    # New Students will be assigned after that, but since Marietta only bills
+    # in July, it won't matter what changes or doesn't - nothing has been
+    # billed
+    # So after April 20, we will just do daily updates.  Whatever she bills
+    # on April 20 will then have to be tracked for new entries and for
+    # room changes.   The process can run daily for the rest of the year
+
     try:
         utcts = fn_get_utcts()
         hashstring = str(utcts) + settings.ADIRONDACK_API_SECRET
@@ -69,14 +80,15 @@ def main():
             "h=" + hash_object.hexdigest() + "&" \
             "AccountCode=2010,2040,2011,2031" \
             + "&" + "Exported=0" \
-            + "&" + "ExportCharges=-1" \
-            + "&" + "STUDENTNUMBER=ASI000987654321"
+            + "&" + "ExportCharges=-1"
+        # \
+        #     + "&" + "STUDENTNUMBER=ASI000987654321"
 
-            # DEFINIIONS
-            # Exported: -1 exported will be included, 0 only non-exported
-            # ExportCharges: if -1 then charges will be marked as exported
+        # DEFINIIONS
+        # Exported: -1 exported will be included, 0 only non-exported
+        # ExportCharges: if -1 then charges will be marked as exported
 
-            # print("URL = " + url)
+        # print("URL = " + url)
 
         response = requests.get(url)
         x = json.loads(response.content)
@@ -84,8 +96,8 @@ def main():
         if not x['DATA']:
             print("No new data found")
         else:
+            # Cleanup previous run CSV files
             files = os.listdir(settings.ADIRONDACK_TXT_OUTPUT)
-
             for f in files:
                 if f.find('misc_housing') != -1:
                     ext = f.find(".csv")
@@ -98,41 +110,94 @@ def main():
                                 settings.ADIRONDACK_ARCHIVED + f[:ext] + "_" +
                                 timestr + f[ext:])
 
+            # How to know if a record has already been processed?
+            # The 'EXPORTED' flag may not be useful.
+            # If it was exported, it does not follow that it has been
+            # processed.  Maybe look for recent records by date
+            # of the incident, process those and skip the others
+            # But how far to look back?
+            # Should I also compare to previous CSV a la ADP?
+            # Or can I use the STUDENTBILLINGINTERNALID number somehow?
+            # Store the numbers in a txt file AFTER the csv has been
+            # successfully created, read that file into a list and if
+            # the new data pulls the same number, pass through
+
+            # testlist = ['2', '968', '069']
+            # Temporary
+            f = open("Assignment_id_lst.txt", "r")
+            contents = f.read()
+            print(contents)
+            testlist = []
+            for row in contents:
+                testlist.append(row[0])
+
             for i in x['DATA']:
+                #--------------------
+                # Validation Option 1
+                rec_id = str(i[16])
+                print("STUDENTBILLINGINTERNALID = " + rec_id)
+                if rec_id in testlist:
+                    print("Matched to list...")
+                #--------------------
+
+
                 # Marietta needs date, description,account number, amount,
                 # ID, totcode, billcode, term
-                rec = []
-                rec.append(i[1])
-                descr = str(i[5])
-                descr = descr.translate(None, '!@#$%.,')
-                rec.append(descr.strip())
-                rec.append("1-003-10041")
-                rec.append(i[2])
-                rec.append(i[0])
-                rec.append("S/A")
-                totcode = i[6]
-                # print(totcode)
-                rec.append(totcode)
-                rec.append(i[4][:2] + i[4][5:])
 
-                fee_file = settings.ADIRONDACK_TXT_OUTPUT + totcode + "_" + \
-                           settings.ADIRONDACK_ROOM_FEES + datetimestr + ".csv"
+                item_date = datetime.strptime(i[1], '%m/%d/%Y')
+                print(item_date)
 
-                # print(fee_file)
-                with codecs.open(fee_file, 'ab',
-                                 encoding='utf-8-sig') as fee_output:
-                    csvWriter = csv.writer(fee_output,
-                                           quoting=csv.QUOTE_NONE)
-                    csvWriter.writerow(rec)
-                fee_output.close()
+                #--------------------
+                # Validation Option 2 uses EXPORTED flag, but is unreliable
+                print(i[9])
+                if i[9] == -1:
+                    print("File was previously exported")
+                else:
+                    print("File fresh")
+                #--------------------
 
-                print("File created, send")
-                SUBJECT = 'Housing Miscellaneous Fees'
-                BODY = 'There are housing fees to process via ASCII post'
-                fn_sendmailfees(settings.ADIRONDACK_TO_EMAIL,
-                                settings.ADIRONDACK_FROM_EMAIL,
-                                BODY, SUBJECT
-                                )
+
+                filter_date = datetime.now() - timedelta(days=90)
+                print(filter_date)
+                if item_date >= filter_date:
+                    print('OK')
+
+                    rec = []
+                    rec.append(i[1])
+                    descr = str(i[5])
+                    descr = descr.translate(None, '!@#$%.,')
+                    rec.append(descr.strip())
+                    rec.append("1-003-10041")
+                    rec.append(i[2])
+                    rec.append(i[0])
+                    rec.append("S/A")
+                    totcode = i[6]
+                    # print(totcode)
+                    rec.append(totcode)
+                    rec.append(i[4][:2] + i[4][5:])
+
+                    fee_file = settings.ADIRONDACK_TXT_OUTPUT + totcode \
+                               + "_" + settings.ADIRONDACK_ROOM_FEES \
+                               + datetimestr + ".csv"
+
+                    # print(fee_file)
+                    with codecs.open(fee_file, 'ab',
+                                     encoding='utf-8-sig') as fee_output:
+                        csvWriter = csv.writer(fee_output,
+                                               quoting=csv.QUOTE_NONE)
+                        csvWriter.writerow(rec)
+                    fee_output.close()
+
+                    print("File created, send")
+                    SUBJECT = 'Housing Miscellaneous Fees'
+                    BODY = 'There are housing fees to process via ASCII post'
+                    # fn_sendmailfees(settings.ADIRONDACK_TO_EMAIL,
+                    #                 settings.ADIRONDACK_FROM_EMAIL,
+                    #                 BODY, SUBJECT
+                    #                 )
+
+                else:
+                    print("OLD Don't process")
 
     except Exception as e:
         print("Error in adirondack_misc_fees_api.py- Main:  " + e.message)
