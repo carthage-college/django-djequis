bbb53739798e269409fc4fff621d68df70be204d
dsullivan <dsullivan@carthage.edu>
2019-09-09 16:09:17 -0500

    minor tweak

--- djequis/adirondack/misc_fees.py
+++ djequis/adirondack/misc_fees.py
@@ -307,7 +307,7 @@ def main():
                 # variables for readability
                 adir_term = i[4][:2] + i[4][-4:]
                 amount = '{:.2f}'.format(i[2])
-                print(amount)
+                # print(amount)
                 # May need to round the amount to 2 decimal places
                 bill_id = str(i[16])
                 stu_id = str(i[0])
