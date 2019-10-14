6854a839da021bdc6384bb0051d3d0b34fa93ef5
plungerman <plungerman@gmail.com>
2019-10-14 08:55:54 -0500

    moved financialaid to djpagan

--- djequis/core/urls.py
+++ djequis/core/urls.py
@@ -2,13 +2,6 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    # admissions
-    #url(
-        #r'^admissions/', include('djequis.core.admissions.urls')
-    #),
-    url(
-        r'^financial-aid/', include('djequis.core.financialaid.urls')
-    ),
     url(
         r'^schoology/', include('djequis.core.schoology.urls')
     ),
